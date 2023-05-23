b6cda316c7192e647b7ffff78d331a0fe2ef7edb
vadymchan <vadymchan@gmail.com>
2023-05-23 21:36:15 +0300

    added implementation for logger (spdlog)

--- include/game_engine/logging/spd_logger.h
+++ include/game_engine/logging/spd_logger.h
@@ -1,26 +1,40 @@
 #pragma once
 #include "logger.h"
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace GameEngine
 {
-	class SPDLogger : public Logger<SPDLogger> 
-	{
-	public:
-		void Debug(const std::string& message) {
-			// Implement SPDLog-specific methods here
-		}
+    class SPDLogger : public Logger<SPDLogger>
+    {
+    public:
+        SPDLogger() {
+            m_logger_ = spdlog::stdout_color_mt("SPDLogger");
+            spdlog::set_pattern("[%T] [%l] %v");
+        }
 
-		void Info(const std::string& message) {
-			// Implement SPDLog-specific methods here
-		}
+        void LogDebug(const std::string& message) 
+        {
+            m_logger_->debug(message);
+        }
 
-		void Warn(const std::string& message) {
-			// Implement SPDLog-specific methods here
-		}
+        void LogInfo(const std::string& message) 
+        {
+            m_logger_->info(message);
+        }
 
-		void Error(const std::string& message) {
-			// Implement SPDLog-specific methods here
-		}
-	};
+        void LogWarn(const std::string& message) 
+        {
+            m_logger_->warn(message);
+        }
+
+        void LogError(const std::string& message) 
+        {
+            m_logger_->error(message);
+        }
+
+    private:
+        std::shared_ptr<spdlog::logger> m_logger_;
+    };
 }
 
