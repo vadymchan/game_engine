837d762d9616d00035332974f78657f5157885ef
vadymchan <vadymchan@gmail.com>
2023-10-26 23:30:58 +0300

    added placeholder to engine class

---
+++ include/engine.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace game_engine {
+
+class Engine {
+  private:
+//TODO: logger
+//TODO: graphics
+//TODO: time (clock, stopwatch)
+
+//TODO: physics
+//TODO: memory manager
+
+	  
+
+};
+
+}  // namespace game_engine
---
+++ src/engine.cpp
@@ -0,0 +1,9 @@
+
+#include "engine.h"
+#include "engine_internal.h"
+
+namespace game_engine {
+
+
+
+} // namespace game_engine
