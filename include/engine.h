e0d55e11539501c47ee93ed59121b2ab45138b2f
vadymchan <vadymchan@gmail.com>
2023-11-11 12:42:16 +0200

    add header guards and todo to engine class

--- include/engine.h
+++ include/engine.h
@@ -1,18 +1,18 @@
-#pragma once
+#ifndef GAME_ENGINE_ENGINE_H
+#define GAME_ENGINE_ENGINE_H
 
 namespace game_engine {
 
 class Engine {
   private:
-//TODO: logger
-//TODO: graphics
-//TODO: time (clock, stopwatch)
+  // TODO: logger
+  // TODO: graphics
+  // TODO: time (clock, stopwatch)
 
-//TODO: physics
-//TODO: memory manager
-
-	  
-
+  // TODO: physics
+  // TODO: memory manager
 };
 
 }  // namespace game_engine
+
+#endif
