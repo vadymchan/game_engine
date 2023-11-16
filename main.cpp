5b753fb130d1a2877a28a2f2be03a78d804bff78
vadymchan <vadymchan@gmail.com>
2023-11-16 19:20:20 +0200

    add sdl test

--- main.cpp
+++ main.cpp
@@ -18,37 +18,102 @@
 //
 //}  // namespace game_engine
 
-#include "utils/logger/global_logger.h"
-#include "utils/logger/console_logger.h"
+#define SDL_MAIN_HANDLED
+
+#include <SDL.h>
+
 
 #include <iostream>
 
-auto main() -> int {
-  // Initialize a console logger
-  auto consoleLogger = std::make_shared<game_engine::ConsoleLogger>(
-      "TestLogger",
-      game_engine::LogLevel::Debug,
-      "%Y-%m-%d %H:%M:%S.%e %^[%l]%$ %v",
-      game_engine::ConsoleStreamType::StdOut,
-      true,
-      true);
+//#include <Windows.h>
+//int WINAPI wWinMain(_In_ HINSTANCE     hInstance,
+//                    _In_opt_ HINSTANCE hPrevInstance,
+//                    _In_ PWSTR         pCmdLine,
+//                    _In_ int           nCmdShow) { 
+  
+int main(int argc, char* argv[]) {
+    // Initialize SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError()
+              << std::endl;
+    return 1;
+  }
 
-  game_engine::ConsoleLogger give_me_a_name;
+  // Create a window
+  SDL_Window* window = SDL_CreateWindow("SDL Tutorial",
+                                        SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED,
+                                        800,
+                                        600,
+                                        SDL_WINDOW_SHOWN);
+  if (window == nullptr) {
+    std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError()
+              << std::endl;
+    SDL_Quit();
+    return 1;
+  }
 
-  // Add the console logger to the global logger
-  game_engine::GlobalLogger::AddLogger(consoleLogger);
+  // Main loop flag
+  bool quit = false;
 
-  // Log messages at different levels
-  game_engine::GlobalLogger::Log(game_engine::LogLevel::Info,
-                                 "This is an info message.");
-  game_engine::GlobalLogger::Log(game_engine::LogLevel::Warning,
-                                 "This is a warning message.");
-  game_engine::GlobalLogger::Log(game_engine::LogLevel::Error,
-                                 "This is an error message.");
+  // Event handler
+  SDL_Event e;
 
-  consoleLogger->log(game_engine::LogLevel::Info, "This is local info message");
+  // While application is running
+  while (!quit) {
+    // Handle events on queue
+    while (SDL_PollEvent(&e) != 0) {
+      // User requests quit
+      if (e.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
 
-  std::cout << "Check the console output for log messages." << '\n';
+    // Your rendering and updating code goes here
+  }
+
+  // Destroy window
+  SDL_DestroyWindow(window);
+
+  // Quit SDL subsystems
+  SDL_Quit();
 
   return 0;
 }
+
+
+//
+//#include "utils/logger/global_logger.h"
+//#include "utils/logger/console_logger.h"
+//
+//#include <iostream>
+//
+//auto main() -> int {
+//  // Initialize a console logger
+//  auto consoleLogger = std::make_shared<game_engine::ConsoleLogger>(
+//      "TestLogger",
+//      game_engine::LogLevel::Debug,
+//      "%Y-%m-%d %H:%M:%S.%e %^[%l]%$ %v",
+//      game_engine::ConsoleStreamType::StdOut,
+//      true,
+//      true);
+//
+//  game_engine::ConsoleLogger give_me_a_name;
+//
+//  // Add the console logger to the global logger
+//  game_engine::GlobalLogger::AddLogger(consoleLogger);
+//
+//  // Log messages at different levels
+//  game_engine::GlobalLogger::Log(game_engine::LogLevel::Info,
+//                                 "This is an info message.");
+//  game_engine::GlobalLogger::Log(game_engine::LogLevel::Warning,
+//                                 "This is a warning message.");
+//  game_engine::GlobalLogger::Log(game_engine::LogLevel::Error,
+//                                 "This is an error message.");
+//
+//  consoleLogger->log(game_engine::LogLevel::Info, "This is local info message");
+//
+//  std::cout << "Check the console output for log messages." << '\n';
+//
+//  return 0;
+//}
