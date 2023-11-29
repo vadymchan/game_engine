0cafed06d8af26e31df868e70337c1ae188c9c4f
vadymchan <vadymchan@gmail.com>
2023-11-29 22:59:57 +0200

    add event class (alias to sdl_event)

---
+++ src/internal_include/event/event.h
@@ -0,0 +1,13 @@
+#ifndef GAME_ENGINE_EVENT_H
+#define GAME_ENGINE_EVENT_H
+
+#include <SDL.h>
+
+namespace game_engine {
+
+using EventType = SDL_EventType;
+using Event     = SDL_Event;
+
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_EVENT_H
