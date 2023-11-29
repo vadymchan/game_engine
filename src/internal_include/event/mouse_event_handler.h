707e5bfffc236687fcf2bfef822173d0779627da
vadymchan <vadymchan@gmail.com>
2023-11-29 23:01:29 +0200

    add mouse event handler (alias to event handler)

---
+++ src/internal_include/event/mouse_event_handler.h
@@ -0,0 +1,10 @@
+#ifndef GAME_ENGINE_EVENT_MOUSE_EVENT_HANDLER_H
+#define GAME_ENGINE_EVENT_MOUSE_EVENT_HANDLER_H
+
+#include "event_handler.h"
+
+namespace game_engine {
+using MouseEventHandler = EventHandler;
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_EVENT_MOUSE_EVENT_HANDLER_H
