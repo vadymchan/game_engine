9b417b9fd07d379e1344f50110dfa6d2eed5060d
vadymchan <vadymchan@gmail.com>
2023-11-29 23:02:05 +0200

    add window event manager (routes window event to handler)

---
+++ src/internal_include/event/window_event_manager.h
@@ -0,0 +1,27 @@
+#ifndef GAME_ENGINE_EVENT_WINDOW_EVENT_MANAGER_H
+#define GAME_ENGINE_EVENT_WINDOW_EVENT_MANAGER_H
+
+#include "event.h"
+#include "window_event_handler.h"
+
+namespace game_engine {
+
+class WindowEventManager {
+  public:
+  WindowEventManager(WindowEventHandler* windowHandler)
+      : m_windowHandler_(windowHandler) {}
+
+  void routeEvent(const Event& event) {
+    if (event.type == SDL_WINDOWEVENT) {
+      m_windowHandler_->dispatch(event);
+    }
+    // TODO: other window event
+  }
+
+  private:
+  WindowEventHandler* m_windowHandler_;
+};
+
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_EVENT_WINDOW_EVENT_MANAGER_H
