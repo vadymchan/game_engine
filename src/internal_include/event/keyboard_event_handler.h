c8eb08ad67e54d1827effcf64e65075da5ceef64
vadymchan <vadymchan@gmail.com>
2023-11-29 23:01:19 +0200

    add keyboard event handler (alias to event handler)

---
+++ src/internal_include/event/keyboard_event_handler.h
@@ -0,0 +1,10 @@
+#ifndef GAME_ENGINE_EVENT_KEYBOARD_EVENT_HANDLER_H
+#define GAME_ENGINE_EVENT_KEYBOARD_EVENT_HANDLER_H
+
+#include "event_handler.h"
+
+namespace game_engine {
+using KeyboardEventHandler = EventHandler;
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_EVENT_KEYBOARD_EVENT_HANDLER_H
