5cc2787189cdd3c7fc8acbdc6198d6e132c2784b
vadymchan <vadymchan@gmail.com>
2023-11-30 14:45:31 +0200

    add window event (alias to sdl types)

--- src/internal_include/event/event.h
+++ src/internal_include/event/event.h
@@ -8,6 +8,9 @@ namespace game_engine {
 using EventType = SDL_EventType;
 using Event     = SDL_Event;
 
+using WindowEvent     = SDL_WindowEvent;
+using WindowEventType = Uint8;
+
 }  // namespace game_engine
 
 #endif  // GAME_ENGINE_EVENT_H
