d39f04f697a82560f4f7eb793f3c6e470c6be702
vadymchan <vadymchan@gmail.com>
2023-11-30 15:13:03 +0200

    incapsulate mouse events inside MouseEventHandler

--- src/internal_include/event/mouse_event_handler.h
+++ src/internal_include/event/mouse_event_handler.h
@@ -9,102 +9,129 @@
 #include <vector>
 
 namespace game_engine {
+
 class MouseEventHandler {
   public:
-  using ButtonEventCallback = std::function<void(const MouseButtonEvent&)>;
-  using MotionEventCallback = std::function<void(const MouseMotionEvent&)>;
-  using WheelEventCallback  = std::function<void(const MouseWheelEvent&)>;
 
-  struct ButtonEventInfo {
-    EventType   m_type;  // SDL_MOUSEBUTTONDOWN or SDL_MOUSEBUTTONUP
-    MouseButton m_button;
+  class ButtonEventHandler {
+    public:
+    struct EventInfo {
+      EventType   m_type;  // SDL_MOUSEBUTTONDOWN or SDL_MOUSEBUTTONUP
+      MouseButton m_button;
+    };
+
+    using EventCallback = std::function<void(const MouseButtonEvent&)>;
+
+    void subscribe(const EventInfo& eventInfo, const EventCallback& callback) {
+      auto& subscribers = (eventInfo.m_type == SDL_MOUSEBUTTONDOWN)
+                            ? m_mouseButtonDownSubscribers
+                            : m_mouseButtonUpSubscribers;
+      subscribers[eventInfo.m_button].emplace_back(callback);
+    }
+
+    void dispatch(const MouseButtonEvent& event) {
+      auto& subscribers = (event.type == SDL_MOUSEBUTTONDOWN)
+                            ? m_mouseButtonDownSubscribers
+                            : m_mouseButtonUpSubscribers;
+      auto  it          = subscribers.find(event.button);
+      if (it != subscribers.end()) {
+        for (auto& callback : it->second) {
+          callback(event);
+        }
+      }
+    }
+
+    private:
+    std::unordered_map<MouseButton, std::vector<EventCallback>>
+        m_mouseButtonDownSubscribers;
+    std::unordered_map<MouseButton, std::vector<EventCallback>>
+        m_mouseButtonUpSubscribers;
   };
 
-  struct MotionEventInfo {
-    EventType        m_type;  // SDL_MOUSEMOTION
-    MouseMotionState m_state;
+  class MotionEventHandler {
+    public:
+    struct EventInfo {
+      EventType        m_type;  // SDL_MOUSEMOTION
+      MouseMotionState m_state;
+    };
+
+    using EventCallback = std::function<void(const MouseMotionEvent&)>;
+
+    void subscribe(const EventInfo& eventInfo, const EventCallback& callback) {
+      if (eventInfo.m_type == SDL_MOUSEMOTION) {
+        m_mouseMotionSubscribers[eventInfo.m_state].emplace_back(callback);
+      }
+    }
+
+    void dispatch(const MouseMotionEvent& event) {
+      auto it = m_mouseMotionSubscribers.find(event.state);
+      if (it != m_mouseMotionSubscribers.end()) {
+        for (auto& callback : it->second) {
+          callback(event);
+        }
+      }
+    }
+
+    private:
+    std::unordered_map<MouseMotionState, std::vector<EventCallback>>
+        m_mouseMotionSubscribers;
   };
 
-  struct WheelEventInfo {
-    EventType m_type;  // SDL_MOUSEWHEEL
+  class WheelEventHandler {
+    public:
+    struct EventInfo {
+      EventType m_type;  // SDL_MOUSEWHEEL
+    };
+
+    using EventCallback = std::function<void(const MouseWheelEvent&)>;
+
+    void subscribe(const EventInfo& eventInfo, const EventCallback& callback) {
+      if (eventInfo.m_type == SDL_MOUSEWHEEL) {
+        m_mouseWheelSubscribers.emplace_back(callback);
+      }
+    }
+
+    void dispatch(const MouseWheelEvent& event) {
+      for (auto& callback : m_mouseWheelSubscribers) {
+        callback(event);
+      }
+    }
+
+    private:
+    std::vector<EventCallback> m_mouseWheelSubscribers;
   };
 
-  void subscribe(ButtonEventInfo            eventType,
-                 const ButtonEventCallback& callback) {
-    auto& subscribers = (eventType.m_type == SDL_MOUSEBUTTONDOWN)
-                          ? m_mouseButtonDownSubscribers_
-                          : m_mouseButtonUpSubscribers_;
-    subscribers[eventType.m_button].emplace_back(callback);
+  void subscribe(const ButtonEventHandler::EventInfo&     eventInfo,
+                 const ButtonEventHandler::EventCallback& callback) {
+    m_buttonEventHandler_.subscribe(eventInfo, callback);
   }
 
-  void subscribe(MotionEventInfo            eventType,
-                 const MotionEventCallback& callback) {
-    if (eventType.m_type == SDL_MOUSEMOTION) {
-      m_mouseMotionSubscribers_[eventType.m_state].emplace_back(callback);
-    }
+  void subscribe(const MotionEventHandler::EventInfo&     eventInfo,
+                 const MotionEventHandler::EventCallback& callback) {
+    m_motionEventHandler_.subscribe(eventInfo, callback);
   }
 
-  void subscribe(WheelEventInfo            eventType,
-                 const WheelEventCallback& callback) {
-    if (eventType.m_type == SDL_MOUSEWHEEL) {
-      m_mouseWheelSubscribers_.emplace_back(callback);
-    }
+  void subscribe(const WheelEventHandler::EventInfo&     eventInfo,
+                 const WheelEventHandler::EventCallback& callback) {
+    m_wheelEventHandler_.subscribe(eventInfo, callback);
   }
 
   void dispatch(const MouseButtonEvent& event) {
-    auto& subscribers = (event.type == SDL_MOUSEBUTTONDOWN)
-                          ? m_mouseButtonDownSubscribers_
-                          : m_mouseButtonUpSubscribers_;
-    auto  it          = subscribers.find(event.button);
-    if (it != subscribers.end()) {
-      for (auto& callback : it->second) {
-        handleEvent_(event, callback);
-      }
-    }
+    m_buttonEventHandler_.dispatch(event);
   }
 
   void dispatch(const MouseMotionEvent& event) {
-    auto& subscribers = m_mouseMotionSubscribers_;
-    auto  it          = subscribers.find(event.state);
-    if (it != subscribers.end()) {
-      for (auto& callback : it->second) {
-        handleEvent_(event, callback);
-      }
-    }
+    m_motionEventHandler_.dispatch(event);
   }
 
   void dispatch(const MouseWheelEvent& event) {
-    auto& subscribers = m_mouseWheelSubscribers_;
-    for (auto& callback : subscribers) {
-      handleEvent_(event, callback);
-    }
+    m_wheelEventHandler_.dispatch(event);
   }
 
   private:
-
-  void handleEvent_(const MouseButtonEvent&         event,
-                    const ButtonEventCallback& callback) {
-    callback(event);
-  }
-
-  void handleEvent_(const MouseMotionEvent&         event,
-                    const MotionEventCallback& callback) {
-    callback(event);
-  }
-
-  void handleEvent_(const MouseWheelEvent&         event,
-                    const WheelEventCallback& callback) {
-    callback(event);
-  }
-
-  std::unordered_map<MouseButton, std::vector<ButtonEventCallback>>
-      m_mouseButtonUpSubscribers_;
-  std::unordered_map<MouseButton, std::vector<ButtonEventCallback>>
-      m_mouseButtonDownSubscribers_;
-  std::unordered_map<MouseMotionState, std::vector<MotionEventCallback>>
-      m_mouseMotionSubscribers_;
-
-  std::vector<WheelEventCallback&> m_mouseWheelSubscribers_;
+  ButtonEventHandler m_buttonEventHandler_;
+  MotionEventHandler m_motionEventHandler_;
+  WheelEventHandler  m_wheelEventHandler_;
 };
 
 }  // namespace game_engine
