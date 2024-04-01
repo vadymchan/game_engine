b9acf25cb4b8fc669266f01f16a158887d0a653a
vadymchan <vadymchan@gmail.com>
2024-04-01 18:16:26 +0300

    add feature macros file

---
+++ src/internal_include/gfx/rhi/vulkan/feature_switch_vk.h
@@ -0,0 +1,11 @@
+#ifndef GAME_ENGINE_FEATURE_SWITCH_VK_H
+#define GAME_ENGINE_FEATURE_SWITCH_VK_H
+
+//#define USE_VARIABLE_SHADING_RATE_TIER2 0
+#define USE_REVERSEZ_PERSPECTIVE_SHADOW 1
+
+// this should be in another file
+#define ENABLE_PBR                      1
+
+
+#endif  // GAME_ENGINE_FEATURE_SWITCH_VK_H
