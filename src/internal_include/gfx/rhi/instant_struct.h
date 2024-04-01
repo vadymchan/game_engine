508f175b3370fdd84add38ad04f5fc8217da4121
vadymchan <vadymchan@gmail.com>
2024-04-01 18:14:58 +0300

    add instant struct file (used for getting hash value from the fields of some class)

---
+++ src/internal_include/gfx/rhi/instant_struct.h
@@ -0,0 +1,69 @@
+#ifndef GAME_ENGINE_INSTANT_STRUCT_H
+#define GAME_ENGINE_INSTANT_STRUCT_H
+
+#include "utils/third_party/xxhash_util.h"
+
+#include <cstdint>
+#include <type_traits>
+
+namespace game_engine {
+
+// TODO: move file to other folder (e.g. utils)
+
+// Struct could be added padding by compiler and it is filled with trash data.
+// so, packing rule set to 1 here. If this code removed, you can see the
+// different hash with same struct data.
+#pragma pack(push, 1)
+
+// InstantStructInternal
+// Recursive declaration of variables that passed into variadic template
+// arguments
+template <typename... Args>
+struct InstantStructInternal {};
+
+template <typename T, typename... Args>
+struct InstantStructInternal<T, Args...>
+    : public InstantStructInternal<Args...> {
+  public:
+  // TODO: currently uncommented, but may cause inconsistend behavior
+  // static_assert(std::is_trivially_copyable<T>::value,
+  //               "InstanceStruct members should be 'trivially copyable'");
+  static_assert(!std::is_pointer<T>::value,
+                "InstanceStruct members should not be 'pointer'");
+  T Data{};
+
+  InstantStructInternal<T, Args...>(T v, Args... args)
+      : Data(v)
+      , InstantStructInternal<Args...>(args...) {}
+};
+
+template <>
+struct InstantStructInternal<> {
+  public:
+};
+
+// InstantStruct
+// A template structure that dynamically generates a struct from the arguments
+// provided to its constructor. This structure aims to create a struct directly
+// from the variables passed to the constructor. It inherits from
+// InstantStructInternal, forwarding the constructor arguments to the base
+// class constructor.
+template <typename... Args>
+struct InstantStruct : public InstantStructInternal<Args...> {
+  InstantStruct(Args... args)
+      : InstantStructInternal<Args...>(args...) {}
+};
+
+#pragma pack(pop)
+
+// Easy hash generation from InstantStruct, it is slow calling 'Hash Generation
+// Function' for each variables Indivisually.
+#define GETHASH_FROM_INSTANT_STRUCT(...)                        \
+  [&]() -> uint64_t {                                           \
+    auto InstanceStruct = InstantStruct(__VA_ARGS__);           \
+    return ::XXH64(&InstanceStruct, sizeof(InstanceStruct), 0); \
+  }()
+
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_INSTANT_STRUCT_H
