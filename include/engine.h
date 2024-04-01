170417252dbc0cc42e8386c258a60f43c2c99d6b
vadymchan <vadymchan@gmail.com>
2024-04-01 20:24:39 +0300

    edit engine class

--- include/engine.h
+++ include/engine.h
@@ -7,6 +7,8 @@
 #include "event/mouse_event_handler.h"
 #include "event/window_event_handler.h"
 #include "event/window_event_manager.h"
+#include "game.h"
+#include "gfx/rhi/vulkan/rhi_vk.h"
 #include "input/input_manager.h"
 #include "platform/common/window.h"
 #include "utils/logger/console_logger.h"
@@ -28,8 +30,10 @@ class Engine {
 
   ~Engine() = default;
 
+  // TODO: add config file
   auto init() -> bool {
     bool successfullyInited{true};
+
 
     // logger
     // ------------------------------------------------------------------------
@@ -38,10 +42,16 @@ class Engine {
 
     // window
     // ------------------------------------------------------------------------
-    m_window_ = std::make_unique<Window>("My Window",
-                                         math::Dimension2Di{800, 600},
-                                         math::Point2Di{100, 100},
-                                         game_engine::Window::Flags::Resizable);
+    m_window_ = std::make_unique<Window>(
+        "First Triangle Vulkan",
+        math::Dimension2Di{800, 600},
+        math::Point2Di{100, 100},
+        game_engine::Window::Flags::Resizable
+            // TODO: Vulkan flag may not work for DX12 window
+            | game_engine::Window::Flags::Vulkan);
+    // game
+    // ------------------------------------------------------------------------
+    m_game_ = std::make_unique<Game>(m_window_);
 
     // input event
     // ------------------------------------------------------------------------
@@ -53,8 +63,12 @@ class Engine {
     // ------------------------------------------------------------------------
     m_windowEventHandler_ = std::make_shared<WindowEventHandler>();
     m_windowEventHandler_->subscribe(
-        SDL_WINDOWEVENT_RESIZED,
-        [this](const WindowEvent& event) { this->m_window_->onResize(event); });
+        SDL_WINDOWEVENT_RESIZED, [this](const WindowEvent& event) {
+          this->m_window_->onResize(event);
+          this->m_game_->Resize(event.data1, event.data2);  // TODO: refactor
+          g_rhi_vk->OnHandleResized(
+              event.data1, event.data2, false);             // TODO: refactor
+        });
     m_windowEventManager_
         = std::make_unique<WindowEventManager>(m_windowEventHandler_);
 
@@ -66,6 +80,12 @@ class Engine {
     // [this](const ApplicationEvent& event) { this->onClose(event); }
     m_applicationEventManager_
         = std::make_unique<ApplicationEventManager>(m_applicationEventHandler_);
+
+    g_rhi_vk = new RhiVk();
+    g_rhi_vk->init(m_window_);
+    g_rhi_vk->OnInitRHI();
+
+    m_game_->Setup();
 
     return successfullyInited;
   }
@@ -79,9 +99,19 @@ class Engine {
       m_deltaTime_.reset();
 
       processEvents_();
+      m_game_->ProcessInput();  // TODO: consider remove
       update_(deltaTime);
+      m_game_->Draw();
       // render();
     }
+  }
+
+  void release() {
+    if (g_rhi_vk) {
+      g_rhi_vk->Flush();
+    }
+
+    m_game_->Release();
   }
 
   void onClose(const ApplicationEvent& event) { m_isRunning_ = false; }
@@ -96,12 +126,16 @@ class Engine {
     }
   }
 
-  void update_(float deltaTime) {}
+  void update_(float deltaTime) {
+    m_game_->Update(deltaTime);
+
+    Shader::StartAndRunCheckUpdateShaderThread();
+  }
 
   bool                           m_isRunning_{false};
   DeltaTime                      m_deltaTime_;
   std::shared_ptr<ConsoleLogger> m_consoleLogger_;
-  std::unique_ptr<Window>        m_window_;
+  std::shared_ptr<Window>        m_window_;
 
   std::shared_ptr<KeyboardEventHandler> m_keyboardEventHandler_;
   std::shared_ptr<MouseEventHandler>    m_mouseEventHandler_;
@@ -113,6 +147,7 @@ class Engine {
   std::shared_ptr<ApplicationEventHandler> m_applicationEventHandler_;
   std::unique_ptr<ApplicationEventManager> m_applicationEventManager_;
 
+  std::unique_ptr<Game> m_game_;
   // TODO: memory manager
 };
 
