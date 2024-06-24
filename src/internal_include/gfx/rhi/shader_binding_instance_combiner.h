fb5bf669d15c3a57ea073f4047346212a7517335
vadymchan <vadymchan@gmail.com>
2024-06-24 10:40:16 +0300

    add shader_binding_instance_combiner.h

--- src/internal_include/gfx/rhi/shader_binding_instance_combiner.h
+++ src/internal_include/gfx/rhi/shader_binding_instance_combiner.h
@@ -6,12 +6,12 @@
 // - cosider move this file to a more appropriate location 
 
 #include "gfx/rhi/resource_container.h"
-#include "gfx/rhi/vulkan/shader_binding_layout_vk.h"
+#include "gfx/rhi/shader_binding_layout.h"
 
 namespace game_engine {
 
 struct ShaderBindingInstanceCombiner {
-  const ShaderBindingInstanceArray* shaderBindingInstanceArray = nullptr;
+  const jShaderBindingInstanceArray* shaderBindingInstanceArray = nullptr;
 
   ResourceContainer<void*>    DescriptorSetHandles;
   ResourceContainer<uint32_t> DynamicOffsets;
