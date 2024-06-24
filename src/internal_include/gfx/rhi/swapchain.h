86d8c1cfd1f1295a542e8829202da614a076e7ab
vadymchan <vadymchan@gmail.com>
2024-06-24 10:41:00 +0300

    add swapchain.h

---
+++ src/internal_include/gfx/rhi/swapchain.h
@@ -0,0 +1,43 @@
+#ifndef GAME_ENGINE_SWAPCHAIN_H
+#define GAME_ENGINE_SWAPCHAIN_H
+
+#include "gfx/rhi/rhi_type.h"
+#include "gfx/rhi/texture.h"
+#include "platform/common/window.h"
+
+#include <math_library/vector.h>
+
+#include <cstdint>
+#include <memory>
+
+namespace game_engine {
+
+class jSwapchainImage {
+  public:
+  virtual ~jSwapchainImage() {}
+
+  virtual void Release() = 0;
+
+  virtual void* GetHandle() const {
+    return TexturePtr ? TexturePtr->GetHandle() : nullptr;
+  }
+
+  std::shared_ptr<jTexture> TexturePtr;
+};
+
+class jSwapchain {
+  public:
+  virtual ~jSwapchain() {}
+
+  virtual bool           Create(const std::shared_ptr<Window>& window)     = 0;
+  virtual void           Release()                                         = 0;
+  virtual void*          GetHandle() const                                 = 0;
+  virtual ETextureFormat GetFormat() const                                 = 0;
+  virtual const math::Dimension2Di& GetExtent() const                      = 0;
+  virtual jSwapchainImage*          GetSwapchainImage(int32_t index) const = 0;
+  virtual int32_t                   GetNumOfSwapchainImages() const        = 0;
+};
+
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_SWAPCHAIN_H
