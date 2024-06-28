560de049aff227bbb1286ecc748a70598d1fb465
vadymchan <vadymchan@gmail.com>
2024-06-28 10:51:06 +0300

    add frame_buffer_pool_vk.h

--- src/internal_include/gfx/rhi/vulkan/frame_buffer_pool_vk.h
+++ src/internal_include/gfx/rhi/vulkan/frame_buffer_pool_vk.h
@@ -1,35 +1,36 @@
-#ifndef GAME_ENGINE_FRAME_BUFFER_POOL_VK_H
-#define GAME_ENGINE_FRAME_BUFFER_POOL_VK_H
-
-#include "gfx/rhi/vulkan/frame_buffer_vk.h"
-
-#include <list>
-#include <map>
-#include <memory>
-
-namespace game_engine {
-class FrameBufferPoolVk {
-  public:
-  FrameBufferPoolVk();
-  ~FrameBufferPoolVk();
-
-  static std::shared_ptr<FrameBufferVk> GetFrameBuffer(
-      const FrameBufferInfoVk& info);
-
-  static void ReturnFrameBuffer(FrameBufferVk* renderTarget);
-
-  static void Release();
-
-  struct FrameBufferPoolResourceVk {
-    bool                           IsUsing = false;
-    std::shared_ptr<FrameBufferVk> FrameBufferPtr;
-  };
-
-  static std::map<size_t, std::list<FrameBufferPoolResourceVk> >
-                                          FrameBufferResourceMap;
-  static std::map<FrameBufferVk*, size_t> FrameBufferHashVariableMap;
-};
-
-}  // namespace game_engine
-
-#endif  // GAME_ENGINE_FRAME_BUFFER_POOL_VK_H
+//#ifndef GAME_ENGINE_FRAME_BUFFER_POOL_VK_H
+//#define GAME_ENGINE_FRAME_BUFFER_POOL_VK_H
+//
+// TODO: consider delete file
+//#include "gfx/rhi/vulkan/frame_buffer_vk.h"
+//
+//#include <list>
+//#include <map>
+//#include <memory>
+//
+//namespace game_engine {
+//class FrameBufferPoolVk {
+//  public:
+//  FrameBufferPoolVk();
+//  ~FrameBufferPoolVk();
+//
+//  static std::shared_ptr<FrameBufferVk> GetFrameBuffer(
+//      const FrameBufferInfoVk& info);
+//
+//  static void ReturnFrameBuffer(FrameBufferVk* renderTarget);
+//
+//  static void Release();
+//
+//  struct FrameBufferPoolResourceVk {
+//    bool                           IsUsing = false;
+//    std::shared_ptr<FrameBufferVk> FrameBufferPtr;
+//  };
+//
+//  static std::map<size_t, std::list<FrameBufferPoolResourceVk> >
+//                                          FrameBufferResourceMap;
+//  static std::map<FrameBufferVk*, size_t> FrameBufferHashVariableMap;
+//};
+//
+//}  // namespace game_engine
+//
+//#endif  // GAME_ENGINE_FRAME_BUFFER_POOL_VK_H
