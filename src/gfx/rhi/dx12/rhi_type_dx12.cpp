c96bc6674191d0078c7f5be20dbe04695dd91283
vadymchan <vadymchan@gmail.com>
2024-08-14 01:42:54 +0300

    minor refactoring (rename classes and structs in rhi_type_dx12.cpp)

--- src/gfx/rhi/dx12/rhi_type_dx12.cpp
+++ src/gfx/rhi/dx12/rhi_type_dx12.cpp
@@ -4,17 +4,17 @@
 
 
 namespace game_engine {
-void jCreatedResource::Free() {
+void CreatedResource::Free() {
   if (Resource) {
-    if (ResourceType == jCreatedResource::EType::Standalone) {
+    if (ResourceType == CreatedResource::EType::Standalone) {
       if (g_rhi_dx12) {
         g_rhi_dx12->DeallocatorMultiFrameStandaloneResource.Free(Resource);
       }
-    } else if (ResourceType == jCreatedResource::EType::ResourcePool) {
+    } else if (ResourceType == CreatedResource::EType::ResourcePool) {
       if (g_rhi_dx12) {
         g_rhi_dx12->DeallocatorMultiFramePlacedResource.Free(Resource);
       }
-    } else if (ResourceType == jCreatedResource::EType::Swapchain) {
+    } else if (ResourceType == CreatedResource::EType::Swapchain) {
       // Nothing to do
     } else {
       assert(0);
