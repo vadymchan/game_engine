040e620ee8728b9b55ea6eb2c9b51c151a119dd5
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:49 +0300

    sort and group elements in semaphore_manager.h according to class_structure.h

--- src/internal_include/gfx/rhi/semaphore_manager.h
+++ src/internal_include/gfx/rhi/semaphore_manager.h
@@ -5,17 +5,33 @@ namespace game_engine {
 
 class ISemaphore {
   public:
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~ISemaphore() {}
 
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
+
   virtual void* getHandle() const = 0;
+
+  // ======= END: public overridden methods   =================================
 };
 
 class ISemaphoreManager {
   public:
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~ISemaphoreManager() {}
 
-  virtual ISemaphore* getOrCreateSemaphore()            = 0;
-  virtual void       returnSemaphore(ISemaphore* fence) = 0;
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
+
+  virtual void        returnSemaphore(ISemaphore* fence) = 0;
+  virtual ISemaphore* getOrCreateSemaphore()             = 0;
+
+  // ======= END: public overridden methods   =================================
 };
 
 }  // namespace game_engine
