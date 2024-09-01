ec6def19aadfb088f4bbab647abf1d4b6ec3c001
vadymchan <vadymchan@gmail.com>
2024-09-01 21:18:27 +0300

    add class_structure.h file as an utility for refactoring comment blocks

---
+++ src/internal_include/utils/refactoring/class_structure.h
@@ -0,0 +1,270 @@
+/**
+ * @file class_structure.h
+ * @brief Utility file for refactoring and reference purposes.
+ *
+ * This file provides a template for organizing class structures into clearly
+ * defined sections. It serves as a utility for refactoring existing code by
+ * enforcing a consistent structure, and as a reference for maintaining
+ * readability and organization in future implementations. The predefined blocks
+ * help in categorizing elements like nested types, aliases, static fields,
+ * methods, and more, making the codebase easier to manage and understand.
+ */
+
+// clang-format off
+
+class ClassStructure {
+  public:
+  // ======= BEGIN: public nested types =======================================
+
+
+
+  // ======= END: public nested types   =======================================
+
+  // ======= BEGIN: public aliases ============================================
+
+
+
+  // ======= END: public aliases   ============================================
+
+  // ======= BEGIN: public static methods =====================================
+
+
+
+  // ======= END: public static methods   =====================================
+
+  // ======= BEGIN: public static fields ======================================
+
+
+
+  // ======= END: public static fields   ======================================
+
+  // ======= BEGIN: public constructors =======================================
+
+
+
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public destructor =========================================
+
+
+
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
+
+
+
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public getters ============================================
+
+
+
+  // ======= END: public getters   ============================================
+
+  // ======= BEGIN: public setters ============================================
+
+
+
+  // ======= END: public setters   ============================================
+
+  // ======= BEGIN: public misc methods =======================================
+
+
+
+  // ======= END: public misc methods   =======================================
+
+  // ======= BEGIN: public overloaded operators ===============================
+
+
+
+  // ======= END: public overloaded operators   ===============================
+
+  // ======= BEGIN: public constants ==========================================
+
+
+
+  // ======= END: public constants   ==========================================
+
+  // ======= BEGIN: public misc fields ========================================
+
+
+
+  // ======= END: public misc fields   ========================================
+
+  protected:
+  // ======= BEGIN: protected nested types ====================================
+
+
+
+  // ======= END: protected nested types   ====================================
+
+  // ======= BEGIN: protected aliases =========================================
+
+
+
+  // ======= END: protected aliases   =========================================
+
+  // ======= BEGIN: protected static methods ==================================
+
+
+
+  // ======= END: protected static methods   ==================================
+  
+  // ======= BEGIN: protected static fields ===================================
+
+
+
+  // ======= END: protected static fields   ===================================
+
+  // ======= BEGIN: protected constructors ====================================
+
+
+
+  // ======= END: protected constructors   ====================================
+
+  // ======= BEGIN: protected destructors ======================================
+
+
+
+  // ======= END: protected destructors   ======================================
+
+  // ======= BEGIN: protected overridden methods ==============================
+
+
+
+  // ======= END: protected overridden methods   ==============================
+
+  // ======= BEGIN: protected getters =========================================
+
+
+
+  // ======= END: protected getters   =========================================
+
+  // ======= BEGIN: protected setters =========================================
+
+
+
+  // ======= END: protected setters   =========================================
+
+  // ======= BEGIN: protected misc methods ====================================
+
+
+
+  // ======= END: protected misc methods   ====================================
+
+  // ======= BEGIN: protected overloaded operators ============================
+
+
+
+  // ======= END: protected overloaded operators   ============================
+
+  // ======= BEGIN: protected constants =======================================
+
+
+
+  // ======= END: protected constants   =======================================
+
+  // ======= BEGIN: protected misc fields =====================================
+
+
+
+  // ======= END: protected misc fields   =====================================
+
+  private:
+  // ======= BEGIN: private friend classes ====================================
+
+
+
+  // ======= END: private friend classes   ====================================
+
+  // ======= BEGIN: private friend functions ==================================
+
+
+
+  // ======= END: private friend functions   ==================================
+
+  // ======= BEGIN: private nested types ======================================
+
+
+
+  // ======= END: private nested types   ======================================
+
+  // ======= BEGIN: private aliases ===========================================
+
+
+
+  // ======= END: private aliases   ===========================================
+
+  // ======= BEGIN: private static methods ====================================
+
+
+
+  // ======= END: private static methods   ====================================
+
+  // ======= BEGIN: private static fields =====================================
+
+
+
+  // ======= END: private static fields   =====================================
+
+  
+  // ======= BEGIN: private constructors ======================================
+
+
+
+  // ======= END: private constructors   ======================================
+
+  // ======= BEGIN: private destructors ========================================
+
+
+
+  // ======= END: private destructors   ========================================
+
+  // ======= BEGIN: private overridden methods ================================
+
+
+
+  // ======= END: private overridden methods   ================================
+
+  // ======= BEGIN: private getters ===========================================
+
+
+
+  // ======= END: private getters   ===========================================
+
+  // ======= BEGIN: private setters ===========================================
+
+
+
+  // ======= END: private setters   ===========================================
+
+  // ======= BEGIN: private misc methods ======================================
+
+
+
+  // ======= END: private misc methods   ======================================
+
+  // ======= BEGIN: private overloaded operators ==============================
+
+
+
+  // ======= END: private overloaded operators   ==============================
+
+  // ======= BEGIN: private constants =========================================
+
+
+
+  // ======= END: private constants   =========================================
+
+  // ======= BEGIN: private misc fields =======================================
+
+
+
+  // ======= END: private misc fields   =======================================
+
+};
+
+// clang-format on
+
+
