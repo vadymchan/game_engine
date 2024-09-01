8d3904bf27eb7b8600f653de7ab84d58d202cb18
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:48 +0300

    sort and group elements in render_pass.h according to class_structure.h

--- src/internal_include/gfx/rhi/render_pass.h
+++ src/internal_include/gfx/rhi/render_pass.h
@@ -14,6 +14,8 @@
 namespace game_engine {
 
 struct Attachment {
+  // ======= BEGIN: public constructors =======================================
+
   Attachment() = default;
 
   Attachment(const std::shared_ptr<RenderTarget>& rtPtr,
@@ -21,9 +23,9 @@ struct Attachment {
              = EAttachmentLoadStoreOp::CLEAR_STORE,
              EAttachmentLoadStoreOp stencilLoadStoreOp
              = EAttachmentLoadStoreOp::CLEAR_STORE,
-             RTClearValue rtClearValue = RTClearValue(0.0f, 0.0f, 0.0f, 1.0f),
+             RtClearValue rtClearValue = RtClearValue(0.0f, 0.0f, 0.0f, 1.0f),
              EResourceLayout initialLayout = EResourceLayout::UNDEFINED,
-             EResourceLayout finalLayout = EResourceLayout::SHADER_READ_ONLY,
+             EResourceLayout finalLayout   = EResourceLayout::SHADER_READ_ONLY,
              bool            isResolveAttachment = false)
       : m_renderTargetPtr_(rtPtr)
       , m_loadStoreOp_(loadStoreOp)
@@ -32,6 +34,42 @@ struct Attachment {
       , m_initialLayout_(initialLayout)
       , m_finalLayout_(finalLayout)
       , m_ResolveAttachment(isResolveAttachment) {}
+
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public getters ============================================
+
+  size_t getHash() const {
+    if (m_hash_) {
+      return m_hash_;
+    }
+
+    m_hash_ = GETHASH_FROM_INSTANT_STRUCT(
+        (m_renderTargetPtr_ ? m_renderTargetPtr_->getHash() : 0),
+        m_loadStoreOp_,
+        m_stencilLoadStoreOp_,
+        m_rtClearValue,
+        m_initialLayout_,
+        m_finalLayout_);
+    return m_hash_;
+  }
+
+  // ======= END: public getters   ============================================
+
+  // ======= BEGIN: public misc methods =======================================
+
+  bool isValid() const { return m_renderTargetPtr_ != nullptr; }
+
+  bool isDepthAttachment() const {
+    assert(m_renderTargetPtr_);
+    return s_isDepthFormat(m_renderTargetPtr_->m_info_.m_format_);
+  }
+
+  bool isResolveAttachment() const { return m_ResolveAttachment; }
+
+  // ======= END: public misc methods   =======================================
+
+  // ======= BEGIN: public misc fields ========================================
 
   std::shared_ptr<RenderTarget> m_renderTargetPtr_;
 
@@ -53,7 +91,7 @@ struct Attachment {
   EAttachmentLoadStoreOp m_stencilLoadStoreOp_
       = EAttachmentLoadStoreOp::CLEAR_STORE;
 
-  RTClearValue m_rtClearValue = RTClearValue(0.0f, 0.0f, 0.0f, 1.0f);
+  RtClearValue m_rtClearValue = RtClearValue(0.0f, 0.0f, 0.0f, 1.0f);
 
   EResourceLayout m_initialLayout_ = EResourceLayout::UNDEFINED;
   EResourceLayout m_finalLayout_   = EResourceLayout::SHADER_READ_ONLY;
@@ -61,78 +99,13 @@ struct Attachment {
   // TODO: consider rename
   bool m_ResolveAttachment = false;
 
-  bool isValid() const { return m_renderTargetPtr_ != nullptr; }
-
-  size_t getHash() const {
-    if (m_hash_) {
-      return m_hash_;
-    }
-
-    m_hash_ = GETHASH_FROM_INSTANT_STRUCT(
-        (m_renderTargetPtr_ ? m_renderTargetPtr_->getHash() : 0),
-        m_loadStoreOp_,
-        m_stencilLoadStoreOp_,
-        m_rtClearValue,
-        m_initialLayout_,
-        m_finalLayout_);
-    return m_hash_;
-  }
-
-  bool isDepthAttachment() const {
-    assert(m_renderTargetPtr_);
-    return s_isDepthFormat(m_renderTargetPtr_->m_info_.m_format_);
-  }
-
-  bool isResolveAttachment() const { return m_ResolveAttachment; }
-
   mutable size_t m_hash_ = 0;
+
+  // ======= END: public misc fields   ========================================
 };
 
 struct Subpass {
-  void initialize(int32_t            sourceSubpassIndex,
-                  int32_t            destSubpassIndex,
-                  EPipelineStageMask attachmentProducePipelineBit,
-                  EPipelineStageMask attachmentConsumePipelineBit) {
-    m_sourceSubpassIndex_           = sourceSubpassIndex;
-    m_destSubpassIndex_             = destSubpassIndex;
-    m_attachmentProducePipelineBit_ = attachmentProducePipelineBit;
-    m_attachmentConsumePipelineBit_ = attachmentConsumePipelineBit;
-  }
-
-  std::vector<int32_t> m_inputAttachments_;
-
-  std::vector<int32_t>   m_outputColorAttachments_;
-  std::optional<int32_t> m_outputDepthAttachment_;
-  std::optional<int32_t> m_outputResolveAttachment_;
-
-  // If both SourceSubpass and DstSubpass of all subpasses are -1, subpasses
-  // will be executed in order
-  int32_t m_sourceSubpassIndex_ = -1;
-  int32_t m_destSubpassIndex_   = -1;
-
-  // Default is the most strong pipeline stage mask
-  EPipelineStageMask m_attachmentProducePipelineBit_ = EPipelineStageMask::
-      COLOR_ATTACHMENT_OUTPUT_BIT;  // The pipeline which attachments would use
-                                    // for this subpass
-  EPipelineStageMask m_attachmentConsumePipelineBit_
-      = EPipelineStageMask::FRAGMENT_SHADER_BIT;  // The pipeline which
-                                                  // attachments would use for
-                                                  // next subapss
-
-  bool isSubpassForExecuteInOrder() const {
-    if ((m_sourceSubpassIndex_ == -1) && (m_destSubpassIndex_ == -1)) {
-      return true;
-    }
-
-    if (m_sourceSubpassIndex_ != -1 && m_destSubpassIndex_ != -1) {
-      return false;
-    }
-
-    // Subpass indices have to be either -1 for all or not for all.
-    // This is an error condition and should be handled appropriately.
-    assert(0);
-    return false;
-  }
+  // ======= BEGIN: public getters ============================================
 
   size_t getHash() const {
     size_t hash = 0;
@@ -161,18 +134,65 @@ struct Subpass {
         &m_attachmentConsumePipelineBit_, sizeof(EPipelineStageMask), hash);
     return hash;
   }
+
+  // ======= END: public getters   ============================================
+
+  // ======= BEGIN: public misc methods =======================================
+
+  bool isSubpassForExecuteInOrder() const {
+    if ((m_sourceSubpassIndex_ == -1) && (m_destSubpassIndex_ == -1)) {
+      return true;
+    }
+
+    if (m_sourceSubpassIndex_ != -1 && m_destSubpassIndex_ != -1) {
+      return false;
+    }
+
+    // Subpass indices have to be either -1 for all or not for all.
+    // This is an error condition and should be handled appropriately.
+    assert(0);
+    return false;
+  }
+
+  void initialize(int32_t            sourceSubpassIndex,
+                  int32_t            destSubpassIndex,
+                  EPipelineStageMask attachmentProducePipelineBit,
+                  EPipelineStageMask attachmentConsumePipelineBit) {
+    m_sourceSubpassIndex_           = sourceSubpassIndex;
+    m_destSubpassIndex_             = destSubpassIndex;
+    m_attachmentProducePipelineBit_ = attachmentProducePipelineBit;
+    m_attachmentConsumePipelineBit_ = attachmentConsumePipelineBit;
+  }
+
+  // ======= END: public misc methods   =======================================
+
+  // ======= BEGIN: public misc fields ========================================
+
+  std::vector<int32_t> m_inputAttachments_;
+
+  std::vector<int32_t>   m_outputColorAttachments_;
+  std::optional<int32_t> m_outputDepthAttachment_;
+  std::optional<int32_t> m_outputResolveAttachment_;
+
+  // If both SourceSubpass and DstSubpass of all subpasses are -1, subpasses
+  // will be executed in order
+  int32_t m_sourceSubpassIndex_ = -1;
+  int32_t m_destSubpassIndex_   = -1;
+
+  // Default is the most strong pipeline stage mask
+  EPipelineStageMask m_attachmentProducePipelineBit_ = EPipelineStageMask::
+      COLOR_ATTACHMENT_OUTPUT_BIT;  // The pipeline which attachments would use
+                                    // for this subpass
+  EPipelineStageMask m_attachmentConsumePipelineBit_
+      = EPipelineStageMask::FRAGMENT_SHADER_BIT;  // The pipeline which
+                                                  // attachments would use for
+                                                  // next subapss
+
+  // ======= END: public misc fields   ========================================
 };
 
 struct RenderPassInfo {
-  std::vector<Attachment> m_attachments_;
-  // TODO: consider remove (currently not used)
-  Attachment              m_resolveAttachment_;
-  std::vector<Subpass>    m_subpasses_;
-
-  void reset() {
-    m_attachments_.clear();
-    m_subpasses_.clear();
-  }
+  // ======= BEGIN: public getters ============================================
 
   size_t getHash() const {
     int64_t hash = 0;
@@ -184,6 +204,15 @@ struct RenderPassInfo {
     }
     return hash;
   }
+
+  void reset() {
+    m_attachments_.clear();
+    m_subpasses_.clear();
+  }
+
+  // ======= END: public getters   ============================================
+
+  // ======= BEGIN: public misc methods =======================================
 
   // If both SourceSubpass and DstSubpass of all subpasses are -1, subpasses
   // will be executed in order
@@ -238,13 +267,31 @@ struct RenderPassInfo {
     }
     return true;
   }
+
+  // ======= END: public misc methods   =======================================
+
+  // ======= BEGIN: public misc fields ========================================
+
+  std::vector<Attachment> m_attachments_;
+  // TODO: consider remove (currently not used)
+  Attachment              m_resolveAttachment_;
+  std::vector<Subpass>    m_subpasses_;
+
+  // ======= END: public misc fields   ========================================
 };
 
 class RenderPass {
   public:
-  virtual ~RenderPass() {}
+  // ======= BEGIN: public constructors =======================================
 
   RenderPass() = default;
+
+  RenderPass(const RenderPassInfo&  renderPassInfo,
+             const math::Vector2Di& offset,
+             const math::Vector2Di& extent) {
+    m_renderPassInfo_ = renderPassInfo;
+    setRenderArea(offset, extent);
+  }
 
   RenderPass(const std::vector<Attachment>& colorAttachments,
              const math::Vector2Di&         offset,
@@ -269,6 +316,34 @@ class RenderPass {
     setAttachemnt(colorAttachments, depthAttachment, colorResolveAttachment);
     setRenderArea(offset, extent);
   }
+
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public destructor =========================================
+
+  virtual ~RenderPass() {}
+
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public getters ============================================
+
+  virtual size_t getHash() const final {
+    if (m_hash_) {
+      return m_hash_;
+    }
+
+    m_hash_ = GETHASH_FROM_INSTANT_STRUCT(
+        m_renderPassInfo_.getHash(), m_renderOffset_, m_renderExtent_);
+    return m_hash_;
+  }
+
+  virtual void* getRenderPass() const { return nullptr; }
+
+  virtual void* getFrameBuffer() const { return nullptr; }
+
+  // ======= END: public getters   ============================================
+
+  // ======= BEGIN: public setters ============================================
 
   void setAttachemnt(const std::vector<Attachment>& colorAttachments) {
     // Add output color attachment
@@ -361,12 +436,9 @@ class RenderPass {
     m_renderExtent_ = extent;
   }
 
-  RenderPass(const RenderPassInfo&  renderPassInfo,
-             const math::Vector2Di& offset,
-             const math::Vector2Di& extent) {
-    m_renderPassInfo_ = renderPassInfo;
-    setRenderArea(offset, extent);
-  }
+  // ======= END: public setters   ============================================
+
+  // ======= BEGIN: public misc methods =======================================
 
   virtual bool beginRenderPass(const CommandBuffer* commandBuffer) {
     return false;
@@ -374,19 +446,9 @@ class RenderPass {
 
   virtual void endRenderPass() {}
 
-  virtual size_t getHash() const final {
-    if (m_hash_) {
-      return m_hash_;
-    }
+  // ======= END: public misc methods   =======================================
 
-    m_hash_ = GETHASH_FROM_INSTANT_STRUCT(
-        m_renderPassInfo_.getHash(), m_renderOffset_, m_renderExtent_);
-    return m_hash_;
-  }
-
-  virtual void* getRenderPass() const { return nullptr; }
-
-  virtual void* getFrameBuffer() const { return nullptr; }
+  // ======= BEGIN: public misc fields ========================================
 
   RenderPassInfo m_renderPassInfo_;
 
@@ -398,6 +460,8 @@ class RenderPass {
   math::Vector2Di m_renderOffset_;
   math::Vector2Di m_renderExtent_;
   mutable size_t  m_hash_ = 0;
+
+  // ======= END: public misc fields   ========================================
 };
 
 }  // namespace game_engine
