6f5ffe91bf5fcab503d0f9bd3838e25872ac07dd
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:44 +0300

    sort and group elements in fence_dx12.h according to class_structure.h

--- src/internal_include/gfx/rhi/dx12/fence_dx12.h
+++ src/internal_include/gfx/rhi/dx12/fence_dx12.h
@@ -11,11 +11,19 @@ namespace game_engine {
 
 class FenceDx12 : public IFence {
   public:
+  // ======= BEGIN: public static fields ======================================
+
   static constexpr uint64_t s_kInitialFenceValue = uint64_t(0);
+
+  // ======= END: public static fields   ======================================
+
+  // ======= BEGIN: public destructor =========================================
 
   virtual ~FenceDx12() override;
 
-  virtual void* getHandle() const override { return m_fence_.Get(); }
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
 
   virtual bool isValid() const override { return m_fence_ && m_fenceEvent_; }
 
@@ -27,19 +35,31 @@ class FenceDx12 : public IFence {
   void waitForFenceValue(uint64_t fenceValue,
                          uint64_t timeoutNanoSec = UINT64_MAX);
 
+  virtual void* getHandle() const override { return m_fence_.Get(); }
+
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public misc methods =======================================
+
   uint64_t signalWithNextFenceValue(ID3D12CommandQueue* commandQueue,
                                     bool waitUntilExecuteComplete = false);
+
+  // ======= END: public misc methods   =======================================
+
+  // ======= BEGIN: public misc fields ========================================
 
   ComPtr<ID3D12Fence> m_fence_;
   HANDLE              m_fenceEvent_ = nullptr;
 
   MutexLock m_fenceValueLock_;
   uint64_t  m_fenceValue_ = s_kInitialFenceValue;
+
+  // ======= END: public misc fields   ========================================
 };
 
 class FenceManagerDx12 : public IFenceManager {
   public:
-  virtual IFence* getOrCreateFence() override;
+  // ======= BEGIN: public overridden methods =================================
 
   virtual void returnFence(IFence* fence) override {
     m_usingFences_.erase(fence);
@@ -48,8 +68,16 @@ class FenceManagerDx12 : public IFenceManager {
 
   virtual void release() override;
 
+  virtual IFence* getOrCreateFence() override;
+
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public misc fields ========================================
+
   std::unordered_set<IFence*> m_usingFences_;
   std::unordered_set<IFence*> m_pendingFences_;
+
+  // ======= END: public misc fields   ========================================
 };
 
 }  // namespace game_engine
