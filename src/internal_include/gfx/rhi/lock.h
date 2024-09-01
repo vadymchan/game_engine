22af8ee7c1bc408ddfb685bd6829ec313ae85d40
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:47 +0300

    sort and group elements in lock.h according to class_structure.h

--- src/internal_include/gfx/rhi/lock.h
+++ src/internal_include/gfx/rhi/lock.h
@@ -20,11 +20,19 @@ namespace game_engine {
 
 class MutexLock {
   public:
+  // ======= BEGIN: public misc methods =======================================
+
   void lock() { m_lock_.lock(); }
 
   void unlock() { m_lock_.unlock(); }
 
+  // ======= END: public misc methods   =======================================
+
+  // ======= BEGIN: public misc fields ========================================
+
   std::mutex m_lock_;
+
+  // ======= END: public misc fields   ========================================
 };
 
 // class RWLock {
@@ -42,6 +50,8 @@ class MutexLock {
 
 class MutexRWLock {
   public:
+  // ======= BEGIN: public misc methods =======================================
+
   void lockRead() { m_lock_.lock_shared(); }
 
   void unlockRead() { m_lock_.unlock_shared(); }
@@ -50,46 +60,88 @@ class MutexRWLock {
 
   void unlockWrite() { m_lock_.unlock(); }
 
+  // ======= END: public misc methods   =======================================
+
+  // ======= BEGIN: public misc fields ========================================
+
   std::shared_mutex m_lock_;
+
+  // ======= END: public misc fields   ========================================
 };
 
 template <typename T>
 class ScopedLock {
   public:
+  // ======= BEGIN: public constructors =======================================
+
   ScopedLock(T* lock)
       : m_scopedLock_(lock) {
     m_scopedLock_->lock();
   }
 
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public destructor =========================================
+
   ~ScopedLock() { m_scopedLock_->unlock(); }
 
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public misc fields ========================================
+
   T* m_scopedLock_;
+
+  // ======= END: public misc fields   ========================================
 };
 
 template <typename T>
 class ScopeReadLock {
   public:
+  // ======= BEGIN: public constructors =======================================
+
   ScopeReadLock(T* lock)
       : m_scopedLock_(lock) {
     m_scopedLock_->lockRead();
   }
 
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public destructor =========================================
+
   ~ScopeReadLock() { m_scopedLock_->unlockRead(); }
 
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public misc fields ========================================
+
   T* m_scopedLock_;
+
+  // ======= END: public misc fields   ========================================
 };
 
 template <typename T>
 class ScopeWriteLock {
   public:
+  // ======= BEGIN: public constructors =======================================
+
   ScopeWriteLock(T* lock)
       : m_scopedLock_(lock) {
     m_scopedLock_->lockWrite();
   }
 
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public destructor =========================================
+
   ~ScopeWriteLock() { m_scopedLock_->unlockWrite(); }
 
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public misc fields ========================================
+
   T* m_scopedLock_;
+
+  // ======= END: public misc fields   ========================================
 };
 
 }  // namespace game_engine
