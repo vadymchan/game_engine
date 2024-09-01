9007e9a3baa8ba807bc7a238cc8ebbcfbba6f752
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:50 +0300

    sort and group elements in command_pool_vk.h according to class_structure.h

--- src/internal_include/gfx/rhi/vulkan/command_pool_vk.h
+++ src/internal_include/gfx/rhi/vulkan/command_pool_vk.h
@@ -1,10 +1,9 @@
 #ifndef GAME_ENGINE_COMMAND_POOL_VK_H
 #define GAME_ENGINE_COMMAND_POOL_VK_H
 
+#include "gfx/rhi/command_buffer_manager.h"
 #include "gfx/rhi/lock.h"
 #include "gfx/rhi/vulkan/command_buffer_vk.h"
-#include "gfx/rhi/command_buffer_manager.h"
-
 
 #include <vulkan/vulkan.h>
 
@@ -15,6 +14,8 @@ namespace game_engine {
 // TODO: consider name CommandPoolVk
 class CommandBufferManagerVk : public ICommandBufferManager {
   public:
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~CommandBufferManagerVk() { releaseInternal(); }
 
   //~CommandBufferManagerVk() {
@@ -27,25 +28,42 @@ class CommandBufferManagerVk : public ICommandBufferManager {
   //  vkDestroyCommandPool(g_rhiVk->m_device_, commandPool, nullptr);
   //}
 
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
+
   virtual bool createPool(uint32_t QueueIndex);
 
   virtual void release() override { releaseInternal(); }
 
-  void releaseInternal();
-
-  const VkCommandPool& getPool() const { return m_commandPool_; }
+  void returnCommandBuffer(CommandBuffer* commandBuffer) override;
 
   virtual CommandBuffer* getOrCreateCommandBuffer() override;
 
-  void returnCommandBuffer(CommandBuffer* commandBuffer) override;
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public getters ============================================
+
+  const VkCommandPool& getPool() const { return m_commandPool_; }
+
+  // ======= END: public getters   ============================================
+
+  // ======= BEGIN: public misc methods =======================================
+
+  void releaseInternal();
+
+  // ======= END: public misc methods   =======================================
 
   private:
-  
+  // ======= BEGIN: private misc fields =======================================
+
   VkCommandPool m_commandPool_;
   MutexLock     m_commandListLock_;
 
   std::vector<CommandBufferVk*> m_usingCommandBuffers_;
   std::vector<CommandBufferVk*> m_availableCommandBuffers_;
+
+  // ======= END: private misc fields   =======================================
 };
 
 }  // namespace game_engine
