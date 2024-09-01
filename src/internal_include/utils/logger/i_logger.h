3475dadcc5ec944932121e98f7fea3a6547afd79
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:54 +0300

    sort and group elements in i_logger.h according to class_structure.h

--- src/internal_include/utils/logger/i_logger.h
+++ src/internal_include/utils/logger/i_logger.h
@@ -17,17 +17,34 @@ enum class LogLevel {
 
 class ILogger {
   public:
-  virtual void log(LogLevel logLevel, const std::string& message) = 0;
+  // ======= BEGIN: public constructors =======================================
 
-  ILogger()                                  = default;
-  ILogger(const ILogger&)                    = delete;
-  auto operator=(const ILogger&) -> ILogger& = delete;
-  ILogger(ILogger&&)                         = delete;
-  auto operator=(ILogger&&) -> ILogger&      = delete;
+  ILogger()               = default;
+  ILogger(const ILogger&) = delete;
+  ILogger(ILogger&&)      = delete;
+
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public destructor =========================================
 
   virtual ~ILogger() = default;
+
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overloaded operators ===============================
+
+  auto operator=(const ILogger&) -> ILogger& = delete;
+  auto operator=(ILogger&&) -> ILogger&      = delete;
+
+  // ======= END: public overloaded operators   ===============================
+
+  // ======= BEGIN: public overridden methods =================================
+
+  virtual void log(LogLevel logLevel, const std::string& message) = 0;
+
+  // ======= END: public overridden methods   =================================
 };
 
 }  // namespace game_engine
 
-#endif // GAME_ENGINE_I_LOGGER_H
+#endif  // GAME_ENGINE_I_LOGGER_H
