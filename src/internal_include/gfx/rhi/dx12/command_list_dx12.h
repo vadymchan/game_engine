ef826e8c5b410462658055aac0ca8a31fa3eaec1
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:43 +0300

    sort and group elements in command_list_dx12.h according to class_structure.h

--- src/internal_include/gfx/rhi/dx12/command_list_dx12.h
+++ src/internal_include/gfx/rhi/dx12/command_list_dx12.h
@@ -8,33 +8,57 @@ namespace game_engine {
 
 // TODO: consider renaming to command list
 struct CommandBufferDx12 : public CommandBuffer {
-  ID3D12GraphicsCommandList4* get() { return m_commandList_.Get(); }
-
-  bool isValid() const { return m_commandList_.Get(); }
-
-  inline ComPtr<ID3D12GraphicsCommandList4>& getRef() { return m_commandList_; }
+  // ======= BEGIN: public overridden methods =================================
 
   virtual bool begin() const override;
   virtual bool end() const override;
   virtual void reset() const override;
 
-  virtual void* getNativeHandle() const override { return m_commandList_.Get(); }
+  virtual void* getNativeHandle() const override {
+    return m_commandList_.Get();
+  }
 
-  virtual void*  getFenceHandle() const override;
-  virtual void   setFence(void* fence) override;
+  virtual void*   getFenceHandle() const override;
   virtual IFence* getFence() const override;
 
+  virtual void setFence(void* fence) override;
+
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public getters ============================================
+
+  inline ComPtr<ID3D12GraphicsCommandList4>& getRef() { return m_commandList_; }
+
+  ID3D12GraphicsCommandList4* get() { return m_commandList_.Get(); }
+
+  // ======= END: public getters   ============================================
+
+  // ======= BEGIN: public misc methods =======================================
+
+  bool isValid() const { return m_commandList_.Get(); }
+
   bool isCompleteForWaitFence();
+
+  // ======= END: public misc methods   =======================================
+
+  // ======= BEGIN: public constants ==========================================
+
+  const class CommandBufferManagerDx12* m_owner_ = nullptr;
+
+  // ======= END: public constants   ==========================================
+
+  // ======= BEGIN: public misc fields ========================================
+
+  class OnlineDescriptorHeapDx12* m_onlineDescriptorHeap_        = nullptr;
+  class OnlineDescriptorHeapDx12* m_onlineSamplerDescriptorHeap_ = nullptr;
 
   ComPtr<ID3D12CommandAllocator>     m_commandAllocator_;
   ComPtr<ID3D12GraphicsCommandList4> m_commandList_;
   mutable bool                       m_isClosed_ = false;
 
-  class OnlineDescriptorHeapDx12* m_onlineDescriptorHeap_        = nullptr;
-  class OnlineDescriptorHeapDx12* m_onlineSamplerDescriptorHeap_ = nullptr;
+  uint64_t m_fenceValue_ = 0;
 
-  const class CommandBufferManagerDx12* m_owner_      = nullptr;
-  uint64_t                              m_fenceValue_ = 0;
+  // ======= END: public misc fields   ========================================
 };
 
 }  // namespace game_engine
