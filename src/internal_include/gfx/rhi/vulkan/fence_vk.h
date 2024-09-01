5b1d53c6bf4fcf8d82d70e5f7dd48f47224ae04f
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:50 +0300

    sort and group elements in fence_vk.h according to class_structure.h

--- src/internal_include/gfx/rhi/vulkan/fence_vk.h
+++ src/internal_include/gfx/rhi/vulkan/fence_vk.h
@@ -13,39 +13,68 @@ namespace game_engine {
 // TODO: override IsComplete methods
 class FenceVk : public IFence {
   public:
+  // ======= BEGIN: public constructors =======================================
+
   FenceVk()
       : m_fence_(VK_NULL_HANDLE) {}
 
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~FenceVk() override;
+
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
+
+  void waitForFence(uint64_t timeout = UINT64_MAX) override;
 
   virtual void* getHandle() const override {
     return reinterpret_cast<void*>(m_fence_);
   }
 
-  void waitForFence(uint64_t timeout = UINT64_MAX) override;
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public misc methods =======================================
 
   // TODO: not used
   void resetFence() const;
 
-  // TODO
-  // private:
-  
+  // ======= END: public misc methods   =======================================
+
+  // ======= BEGIN: public misc fields ========================================
+
   VkFence m_fence_;
+
+  // ======= END: public misc fields   ========================================
 };
 
 class FenceManagerVk : public IFenceManager {
   public:
+  // ======= BEGIN: public destructor =========================================
+
   ~FenceManagerVk() { release(); }
 
-  IFence* getOrCreateFence() override;
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
 
   void returnFence(IFence* fence) override;
 
   void release() override;
 
+  IFence* getOrCreateFence() override;
+
+  // ======= END: public overridden methods   =================================
+
   private:
+  // ======= BEGIN: private misc fields =======================================
+
   std::unordered_set<IFence*> m_usingFences_;
   std::unordered_set<IFence*> m_pendingFences_;
+
+  // ======= END: private misc fields   =======================================
 };
 
 }  // namespace game_engine
