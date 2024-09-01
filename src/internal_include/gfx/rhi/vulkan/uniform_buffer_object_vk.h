9d607a90d943b4dd659f73a19c9aeacd22830106
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:53 +0300

    sort and group elements in uniform_buffer_object_vk.h according to class_structure.h

--- src/internal_include/gfx/rhi/vulkan/uniform_buffer_object_vk.h
+++ src/internal_include/gfx/rhi/vulkan/uniform_buffer_object_vk.h
@@ -10,10 +10,21 @@ namespace game_engine {
 
 // TODO: add interface IUniformBufferBlock
 struct UniformBufferBlockVk : public IUniformBufferBlock {
+  // ======= BEGIN: public aliases ============================================
+
+  // TODO: consider whether this can be treated as alias
   using IUniformBufferBlock::IUniformBufferBlock;
   using IUniformBufferBlock::updateBufferData;
 
+  // ======= END: public aliases   ============================================
+
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~UniformBufferBlockVk() {}
+
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
 
   virtual void init(size_t size) override;
 
@@ -25,19 +36,43 @@ struct UniformBufferBlockVk : public IUniformBufferBlock {
 
   virtual void clearBuffer(int32_t clearValue) override;
 
-  virtual void* getLowLevelResource() const override { return m_buffer_.m_buffer_; }
+  virtual void* getLowLevelResource() const override {
+    return m_buffer_.m_buffer_;
+  }
 
-  virtual void* getLowLevelMemory() const override { return m_buffer_.m_deviceMemory_; }
+  virtual void* getLowLevelMemory() const override {
+    return m_buffer_.m_deviceMemory_;
+  }
 
-  virtual size_t getBufferSize() const override { return m_buffer_.m_allocatedSize_; }
+  virtual size_t getBufferSize() const override {
+    return m_buffer_.m_allocatedSize_;
+  }
 
-  virtual size_t getBufferOffset() const override { return m_buffer_.m_offset_; }
+  virtual size_t getBufferOffset() const override {
+    return m_buffer_.m_offset_;
+  }
+
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public misc fields ========================================
 
   BufferVk m_buffer_;
 
+  // ======= END: public misc fields   ========================================
+
   private:
-  UniformBufferBlockVk(const UniformBufferBlockVk&)            = delete;
+  // ======= BEGIN: public constructors =======================================
+
+  // TODO: this is probably need to be placed in public
+  UniformBufferBlockVk(const UniformBufferBlockVk&) = delete;
+
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public overloaded operators ===============================
+
   UniformBufferBlockVk& operator=(const UniformBufferBlockVk&) = delete;
+
+  // ======= END: public overloaded operators   ===============================
 };
 
 }  // namespace game_engine
