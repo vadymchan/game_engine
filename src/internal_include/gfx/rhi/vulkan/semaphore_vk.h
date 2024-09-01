09716aa76eb78a2841f672e9b5449a19e3e772b9
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:52 +0300

    sort and group elements in semaphore_vk.h according to class_structure.h

--- src/internal_include/gfx/rhi/vulkan/semaphore_vk.h
+++ src/internal_include/gfx/rhi/vulkan/semaphore_vk.h
@@ -12,32 +12,62 @@ namespace game_engine {
 
 class SemaphoreVk : public ISemaphore {
   public:
+  // ======= BEGIN: public constructors =======================================
+
   SemaphoreVk()
       : m_semaphore_(VK_NULL_HANDLE) {}
 
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~SemaphoreVk();
 
-  virtual void* getHandle() const { return reinterpret_cast<void*>(m_semaphore_); }
+  // ======= END: public destructor   =========================================
 
-  // TODO
-  // private:
+  // ======= BEGIN: public overridden methods =================================
+
+  virtual void* getHandle() const {
+    return reinterpret_cast<void*>(m_semaphore_);
+  }
+
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public misc fields ========================================
 
   VkSemaphore m_semaphore_;
+
+  // ======= END: public misc fields   ========================================
 };
 
 class SemaphoreManagerVk : public ISemaphoreManager {
   public:
+  // ======= BEGIN: public destructor =========================================
+
   ~SemaphoreManagerVk() { release(); }
 
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
+
+  void        returnSemaphore(ISemaphore* semaphore) override;
   ISemaphore* getOrCreateSemaphore() override;
 
-  void returnSemaphore(ISemaphore* semaphore) override;
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public misc methods =======================================
 
   void release();
 
+  // ======= END: public misc methods   =======================================
+
   private:
+  // ======= BEGIN: private misc fields =======================================
+
   std::unordered_set<ISemaphore*> m_usingSemaphores_;
   std::unordered_set<ISemaphore*> m_pendingSemaphores_;
+
+  // ======= END: private misc fields   =======================================
 };
 
 }  // namespace game_engine
