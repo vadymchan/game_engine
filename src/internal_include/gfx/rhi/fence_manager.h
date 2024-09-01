c63048012dd12eddfe35bae28551eeaa28482d98
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:46 +0300

    sort and group elements in fence_manager.h according to class_structure.h

--- src/internal_include/gfx/rhi/fence_manager.h
+++ src/internal_include/gfx/rhi/fence_manager.h
@@ -7,11 +7,16 @@ namespace game_engine {
 
 class IFence {
   public:
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~IFence() {}
 
-  virtual void* getHandle() const = 0;
-  // virtual void  release()                                          = 0;
-  virtual void  waitForFence(uint64_t timeoutNanoSec = UINT64_MAX) = 0;
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
+
+  // virtual void  s_release()                                     = 0;
+  virtual void waitForFence(uint64_t timeoutNanoSec = UINT64_MAX) = 0;
 
   // TODO: consider making them pure virtual
   virtual bool isValid() const { return false; }
@@ -19,15 +24,27 @@ class IFence {
   virtual bool isComplete(uint64_t fenceValue) const { return false; }
 
   virtual bool isComplete() const { return false; }
+
+  virtual void* getHandle() const = 0;
+
+  // ======= END: public overridden methods   =================================
 };
 
 class IFenceManager {
   public:
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~IFenceManager() {}
 
-  virtual IFence* getOrCreateFence()        = 0;
-  virtual void   returnFence(IFence* fence) = 0;
-  virtual void   release()                 = 0;
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
+
+  virtual void    returnFence(IFence* fence) = 0;
+  virtual void    release()                  = 0;
+  virtual IFence* getOrCreateFence()         = 0;
+
+  // ======= END: public overridden methods   =================================
 };
 
 }  // namespace game_engine
