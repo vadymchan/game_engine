b879e43aa0f9a575b7c477cd8ebc9d0293230328
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:47 +0300

    sort and group elements in render_frame_context.h according to class_structure.h

--- src/internal_include/gfx/rhi/render_frame_context.h
+++ src/internal_include/gfx/rhi/render_frame_context.h
@@ -10,40 +10,61 @@ namespace game_engine {
 
 struct RenderFrameContext
     : public std::enable_shared_from_this<RenderFrameContext> {
+  // ======= BEGIN: public nested types =======================================
+
+  // ======= END: public nested types   =======================================
   enum ECurrentRenderPass {
     None,
     ShadowPass,
     BasePass,
   };
 
+  // ======= BEGIN: public constructors =======================================
+
   RenderFrameContext() = default;
 
   RenderFrameContext(CommandBuffer* commandBuffer)
       : m_commandBuffer_(commandBuffer) {}
 
+  // ======= END: public constructors   =======================================
+
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~RenderFrameContext();
 
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
+
   virtual void destroy();
-
-  virtual CommandBuffer* getActiveCommandBuffer() const {
-    return m_commandBuffer_;
-  }
-
   virtual bool beginActiveCommandBuffer();
   virtual bool endActiveCommandBuffer();
 
   virtual void submitCurrentActiveCommandBuffer(
       ECurrentRenderPass currentRenderPass) {}
 
-  public:
-  // RaytracingScene*                raytracingScene            = nullptr;
+  virtual CommandBuffer* getActiveCommandBuffer() const {
+    return m_commandBuffer_;
+  }
+
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public misc fields ========================================
+
+  // RaytracingScene*                m_raytracingScene_            = nullptr;
   std::shared_ptr<SceneRenderTarget> m_sceneRenderTargetPtr_       = nullptr;
   uint32_t                           m_frameIndex_                 = -1;
   bool                               m_useForwardRenderer_         = true;
   bool                               m_isBeginActiveCommandbuffer_ = false;
 
+  // ======= END: public misc fields   ========================================
+
   protected:
+  // ======= BEGIN: protected misc fields =====================================
+
   CommandBuffer* m_commandBuffer_ = nullptr;
+
+  // ======= END: protected misc fields   =====================================
 };
 
 }  // namespace game_engine
