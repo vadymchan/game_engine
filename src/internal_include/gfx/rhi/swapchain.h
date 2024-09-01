3ef8a498c8ec62f96541bd2d798ee7759636dfc8
vadymchan <vadymchan@gmail.com>
2024-09-01 23:31:49 +0300

    sort and group elements in swapchain.h according to class_structure.h

--- src/internal_include/gfx/rhi/swapchain.h
+++ src/internal_include/gfx/rhi/swapchain.h
@@ -14,7 +14,13 @@ namespace game_engine {
 
 class ISwapchainImage {
   public:
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~ISwapchainImage() {}
+
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
 
   virtual void release() = 0;
 
@@ -22,20 +28,34 @@ class ISwapchainImage {
     return m_TexturePtr_ ? m_TexturePtr_->getHandle() : nullptr;
   }
 
+  // ======= END: public overridden methods   =================================
+
+  // ======= BEGIN: public misc fields ========================================
+
   std::shared_ptr<Texture> m_TexturePtr_;
+
+  // ======= END: public misc fields   ========================================
 };
 
 class ISwapchain {
   public:
+  // ======= BEGIN: public destructor =========================================
+
   virtual ~ISwapchain() {}
+
+  // ======= END: public destructor   =========================================
+
+  // ======= BEGIN: public overridden methods =================================
 
   virtual bool           create(const std::shared_ptr<Window>& window)     = 0;
   virtual void           release()                                         = 0;
   virtual void*          getHandle() const                                 = 0;
   virtual ETextureFormat getFormat() const                                 = 0;
   virtual const math::Dimension2Di& getExtent() const                      = 0;
-  virtual ISwapchainImage*           getSwapchainImage(int32_t index) const = 0;
+  virtual ISwapchainImage*          getSwapchainImage(int32_t index) const = 0;
   virtual int32_t                   getNumOfSwapchainImages() const        = 0;
+
+  // ======= END: public overridden methods   =================================
 };
 
 }  // namespace game_engine
