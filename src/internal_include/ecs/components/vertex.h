88f9c3a4a51ecede696b738d0b793245f51bc7a1
vadymchan <vadymchan@gmail.com>
2024-12-24 00:32:15 +0200

    Add vertex.h
    Declares vertex structure for rendering pipelines.

---
+++ src/internal_include/ecs/components/vertex.h
@@ -0,0 +1,19 @@
+#ifndef GAME_ENGINE_VERTEX_H
+#define GAME_ENGINE_VERTEX_H
+
+#include <math_library/vector.h>
+
+namespace game_engine {
+
+struct Vertex {
+  math::Vector3Df position;
+  math::Vector3Df normal;
+  math::Vector2Df texCoords;
+  math::Vector3Df tangent;
+  math::Vector3Df bitangent;
+  math::Vector4Df color;
+};
+
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_VERTEX_H
