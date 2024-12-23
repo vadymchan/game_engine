0b8c3611e054b74f0e772a51131b1317e718de4f
vadymchan <vadymchan@gmail.com>
2024-12-24 00:32:14 +0200

    Add mesh.h
    Defines the ECS mesh component.

---
+++ src/internal_include/ecs/components/mesh.h
@@ -0,0 +1,20 @@
+#ifndef GAME_ENGINE_MESH_H
+#define GAME_ENGINE_MESH_H
+
+#include "ecs/components/vertex.h"
+
+#include <string>
+#include <vector>
+
+namespace game_engine {
+
+// This is the geometry data on CPU side (imported from assimp)
+struct Mesh {
+  std::string           meshName;
+  std::vector<Vertex>   vertices;
+  std::vector<uint32_t> indices;
+};
+
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_MESH_H
