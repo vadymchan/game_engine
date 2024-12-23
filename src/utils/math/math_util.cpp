464342b1ade09998a5de566787c61afba27aadb2
vadymchan <vadymchan@gmail.com>
2024-12-24 00:32:20 +0200

    Add math_util.cpp
    Collection of math utility functions.

---
+++ src/utils/math/math_util.cpp
@@ -0,0 +1,13 @@
+#include "utils\math\math_util.h"
+
+namespace math {
+
+Vector3Df g_getVectorfromConfig(const game_engine::ConfigValue& value) {
+  Vector3Df result;
+  result.x() = value["x"].GetFloat();
+  result.y() = value["y"].GetFloat();
+  result.z() = value["z"].GetFloat();
+  return result;
+}
+
+}  // namespace math
