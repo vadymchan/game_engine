587c59838b910ea25d1d482080d473becfcac87f
vadymchan <vadymchan@gmail.com>
2024-12-24 00:32:13 +0200

    Add runtime_settings.h
    Header for runtime settings definitions.

---
+++ src/internal_include/config/runtime_settings.h
@@ -0,0 +1,30 @@
+#ifndef GAME_ENGINE_RUNTIME_SETTINGS_H
+#define GAME_ENGINE_RUNTIME_SETTINGS_H
+
+#include "config/config_manager.h"
+#include "utils/path_manager/path_manager.h"
+#include "utils/service/service_locator.h"
+
+#include <math_library/vector.h>
+
+namespace game_engine {
+
+class RuntimeSettings {
+  public:
+  static RuntimeSettings& s_get();
+
+  RuntimeSettings(const RuntimeSettings&)            = delete;
+  RuntimeSettings& operator=(const RuntimeSettings&) = delete;
+
+  const math::Vector3Df& getWorldUp() const;
+
+  void updateFromConfig();
+
+  private:
+  RuntimeSettings();
+  math::Vector3Df m_worldUp_;
+};
+
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_RUNTIME_SETTINGS_H
