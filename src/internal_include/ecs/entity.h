f237c9e8372432b59d5d8790a6ccf772bc6f74bb
vadymchan <vadymchan@gmail.com>
2024-12-24 00:32:15 +0200

    Add entity.h
    Header defining the core ECS Entity structure.

---
+++ src/internal_include/ecs/entity.h
@@ -0,0 +1,12 @@
+#ifndef GAME_ENGINE_ENTITY_H
+#define GAME_ENGINE_ENTITY_H
+
+#include <entt/entt.hpp>
+
+namespace game_engine {
+
+using Entity = entt::entity;
+
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_ENTITY_H
