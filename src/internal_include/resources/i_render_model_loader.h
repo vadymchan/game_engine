e53e974b4ceb9e4f815fe74affa72a74c857dc4f
vadymchan <vadymchan@gmail.com>
2024-12-24 00:32:17 +0200

    Add i_render_model_loader.h
    Interface for render model loaders.

---
+++ src/internal_include/resources/i_render_model_loader.h
@@ -0,0 +1,31 @@
+#ifndef GAME_ENGINE_I_RENDER_MODEL_LOADER_H
+#define GAME_ENGINE_I_RENDER_MODEL_LOADER_H
+
+#include "ecs/components/model.h"
+#include "ecs/components/render_model.h"
+
+#include <optional>
+
+namespace game_engine {
+
+// we made RenderModelLoader friend to ModelLoader and MaterialLoader so we can
+// access. As an alternative we can expose private methods to public.
+
+// TODO: consider merge RenderModelLoader and ModelLoader (since this may
+// confuse users)
+class IRenderModelLoader {
+  public:
+  virtual ~IRenderModelLoader() = default;
+
+  // Optionally returns CPU-side geometry model
+  // TODO: check that std::optional works. if not, use std::shared_ptr<Model>*
+  // instead
+  virtual std::shared_ptr<RenderModel> loadRenderModel(
+      const std::filesystem::path&          filePath,
+      std::optional<std::shared_ptr<Model>> outModel = std::nullopt)
+      = 0;
+};
+
+}  // namespace game_engine
+
+#endif  // GAME_ENGINE_I_RENDER_MODEL_LOADER_H
