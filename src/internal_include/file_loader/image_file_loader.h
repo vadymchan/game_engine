fa9c8ba28bc70b35e6c9deba4f3c2cec147c87cf
vadymchan <vadymchan@gmail.com>
2024-12-25 00:41:03 +0200

    Update image_file_loader.h
    Added new function for async image loading.

--- src/internal_include/file_loader/image_file_loader.h
+++ src/internal_include/file_loader/image_file_loader.h
@@ -5,6 +5,7 @@
 #include "gfx/rhi/rhi_type.h"
 #include "resources/image.h"
 
+// TODO: remove
 #include <vulkan/vulkan.h>
 
 #include <filesystem>
@@ -17,10 +18,11 @@ class IImageLoader {
   virtual std::shared_ptr<Image> loadImage(
       const std::filesystem::path& filepath)
       = 0;
+  virtual bool supportsFormat(const std::string& extension) const = 0;
 };
 
 // TODO: consider removing code below
-struct ImageSubResourceData {
+struct ImageSubResourceDataDeprecated {
   // ======= BEGIN: public misc fields ========================================
 
   int32_t  m_format_   = 0;
@@ -34,30 +36,30 @@ struct ImageSubResourceData {
   // ======= END: public misc fields   ========================================
 };
 
-struct ImageBulkData {
+struct ImageBulkDataDeprecated {
   // ======= BEGIN: public misc fields ========================================
 
-  std::vector<unsigned char>        m_imageData_;
-  std::vector<ImageSubResourceData> m_subresourceFootprints_;
+  std::vector<unsigned char>                  m_imageData_;
+  std::vector<ImageSubResourceDataDeprecated> m_subresourceFootprints_;
 
   // ======= END: public misc fields   ========================================
 };
 
-struct ImageData {
+struct ImageDataDeprecated {
   // ======= BEGIN: public misc fields ========================================
 
-  bool           m_createMipmapIfPossible_ = true;
-  bool           m_sRGB_                   = false;
-  int32_t        m_width_                  = 0;
-  int32_t        m_height_                 = 0;
-  int32_t        m_mipLevel_               = 1;
-  int32_t        m_layerCount_             = 1;
+  bool                    m_createMipmapIfPossible_ = true;
+  bool                    m_sRGB_                   = false;
+  int32_t                 m_width_                  = 0;
+  int32_t                 m_height_                 = 0;
+  int32_t                 m_mipLevel_               = 1;
+  int32_t                 m_layerCount_             = 1;
   // TODO: currently not used
-  Name           m_filename_;
-  ETextureFormat m_format_      = ETextureFormat::RGBA8;
-  EFormatType    m_formatType_  = EFormatType::UNSIGNED_BYTE;
-  ETextureType   m_textureType_ = ETextureType::TEXTURE_2D;
-  ImageBulkData  m_imageBulkData_;  // TODO: ImageBulkData rhi_vk
+  Name                    m_filename_;
+  ETextureFormat          m_format_      = ETextureFormat::RGBA8;
+  EFormatType             m_formatType_  = EFormatType::UNSIGNED_BYTE;
+  ETextureType            m_textureType_ = ETextureType::TEXTURE_2D;
+  ImageBulkDataDeprecated m_imageBulkData_;  // TODO: ImageBulkData rhi_vk
 
   // ======= END: public misc fields   ========================================
 };
