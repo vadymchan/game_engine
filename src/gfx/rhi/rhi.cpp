760c7c956b9019e8a1d054d0fe26ba74a8abbc34
vadymchan <vadymchan@gmail.com>
2024-12-25 00:41:01 +0200

    Revise rhi.cpp
    Refined RHI initialization and cross-platform checks.

--- src/gfx/rhi/rhi.cpp
+++ src/gfx/rhi/rhi.cpp
@@ -13,7 +13,7 @@ std::shared_ptr<Texture>  g_whiteTexture;
 std::shared_ptr<Texture>  g_blackTexture;
 std::shared_ptr<Texture>  g_whiteCubeTexture;
 std::shared_ptr<Texture>  g_normalTexture;
-std::shared_ptr<Material> g_defaultMaterial = nullptr;
+std::shared_ptr<MaterialOld> g_defaultMaterial = nullptr;
 
 TResourcePool<Shader, MutexRWLock> RHI::s_shaderPool;
 
@@ -24,7 +24,7 @@ bool RHI::init(const std::shared_ptr<Window>& window) {
 }
 
 void RHI::onInitRHI() {
-  ImageData image;
+  ImageDataDeprecated image;
   image.m_imageBulkData_.m_imageData_ = {255, 255, 255, 255};
   image.m_width_                   = 1;
   image.m_height_                  = 1;
@@ -33,24 +33,24 @@ void RHI::onInitRHI() {
   image.m_sRGB_                    = false;
 
   image.m_imageBulkData_.m_imageData_ = {255, 255, 255, 255};
-  g_whiteTexture                 = createTextureFromData(&image);
+  g_whiteTexture                 = createTextureFromDataDeprecated(&image);
 
   image.m_imageBulkData_.m_imageData_ = {0, 0, 0, 255};
-  g_blackTexture                 = createTextureFromData(&image);
+  g_blackTexture                 = createTextureFromDataDeprecated(&image);
 
   image.m_imageBulkData_.m_imageData_ = {255 / 2, 255 / 2, 255, 0};
-  g_normalTexture                = createTextureFromData(&image);
+  g_normalTexture                = createTextureFromDataDeprecated(&image);
 
   image.m_textureType_             = ETextureType::TEXTURE_CUBE;
   image.m_layerCount_              = 6;
   image.m_imageBulkData_.m_imageData_ = {255, 255, 255, 200, 255, 255};
-  g_whiteCubeTexture             = createTextureFromData(&image);
+  g_whiteCubeTexture             = createTextureFromDataDeprecated(&image);
 
-  g_defaultMaterial = std::make_shared<Material>();
-  g_defaultMaterial->m_texData_[(int32_t)Material::EMaterialTextureType::Albedo]
+  g_defaultMaterial = std::make_shared<MaterialOld>();
+  g_defaultMaterial->m_texData_[(int32_t)MaterialOld::EMaterialTextureType::Albedo]
       .m_texture_
       = g_whiteTexture.get();
-  g_defaultMaterial->m_texData_[(int32_t)Material::EMaterialTextureType::Normal]
+  g_defaultMaterial->m_texData_[(int32_t)MaterialOld::EMaterialTextureType::Normal]
       .m_texture_
       = g_normalTexture.get();
 }
