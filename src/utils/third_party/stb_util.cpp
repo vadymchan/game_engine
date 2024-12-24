955bdb6215ec31bb6c4806a276a6b1f4128e8566
vadymchan <vadymchan@gmail.com>
2024-12-25 00:41:10 +0200

    Refactor stb_util.cpp
    Consolidated STB image handling and improved error checks.

--- src/utils/third_party/stb_util.cpp
+++ src/utils/third_party/stb_util.cpp
@@ -7,36 +7,24 @@
 
 namespace game_engine {
 
-bool STBImageLoader::isSupportedImageFormat_(
-    const std::filesystem::path& filepath) const {
-  static const std::unordered_set<std::string_view> supportedExtensions
-      = {".jpeg",
-         ".jpg",
-         ".png",
-         ".bmp",
-         ".tga",
-         ".gif",
-         ".hdr",
-         ".pic",
-         ".ppm",
-         ".pgm"};
+const std::unordered_set<std::string> STBImageLoader::supportedExtensions_
+    = {".jpeg",
+       ".jpg",
+       ".png",
+       ".bmp",
+       ".tga",
+       ".gif",
+       ".hdr",
+       ".pic",
+       ".ppm",
+       ".pgm"};
 
-  std::string extension = filepath.extension().string();
-  std::transform(
-      extension.begin(), extension.end(), extension.begin(), ::tolower);
-
-  return supportedExtensions.find(extension) != supportedExtensions.end();
+bool STBImageLoader::supportsFormat(const std::string& extension) const {
+  return supportedExtensions_.find(extension) != supportedExtensions_.end();
 }
 
 std::shared_ptr<Image> STBImageLoader::loadImage(
     const std::filesystem::path& filepath) {
-  if (!isSupportedImageFormat_(filepath)) {
-    // TODO: use logger
-    std::cerr << "Unsupported image format: " << filepath.extension().string()
-              << std::endl;
-    return nullptr;
-  }
-
   if (stbi_is_hdr(filepath.string().c_str())) {
     return loadImageData_(filepath, &loadHdr_, stbi_image_free, 32, true);
   } else if (stbi_is_16_bit(filepath.string().c_str())) {
@@ -73,6 +61,47 @@ void* STBImageLoader::load8Bit_(const std::filesystem::path& filepath,
       filepath.string().c_str(), x, y, channelsInFile, desiredChannels));
 }
 
+ETextureFormat STBImageLoader::determineFormat_(int32_t channels,
+                                                int32_t bitsPerChannel,
+                                                bool    isHdr) {
+  if (channels == 1) {
+    if (bitsPerChannel == 8 && !isHdr) {
+      return ETextureFormat::R8;
+    } else if (bitsPerChannel == 16 && !isHdr) {
+      return ETextureFormat::R16F;
+    } else if (bitsPerChannel == 32 || isHdr) {
+      return ETextureFormat::R32F;
+    }
+  } else if (channels == 2) {
+    if (bitsPerChannel == 8 && !isHdr) {
+      return ETextureFormat::RG8;
+    } else if (bitsPerChannel == 16 && !isHdr) {
+      return ETextureFormat::RG16F;
+    } else if (bitsPerChannel == 32 || isHdr) {
+      return ETextureFormat::RG32F;
+    }
+  } else if (channels == 3) {
+    if (bitsPerChannel == 8 && !isHdr) {
+      return ETextureFormat::RGB8;
+    } else if (bitsPerChannel == 16 && !isHdr) {
+      return ETextureFormat::RGB16F;
+    } else if (bitsPerChannel == 32 || isHdr) {
+      return ETextureFormat::RGB32F;
+    }
+  } else if (channels == 4) {
+    if (bitsPerChannel == 8 && !isHdr) {
+      return ETextureFormat::RGBA8;
+    } else if (bitsPerChannel == 16 && !isHdr) {
+      return ETextureFormat::RGBA16F;
+    } else if (bitsPerChannel == 32 || isHdr) {
+      return ETextureFormat::RGBA32F;
+    }
+  }
+
+  // TODO: log error
+  return ETextureFormat::MAX;
+}
+
 std::shared_ptr<Image> STBImageLoader::loadImageData_(
     const std::filesystem::path& filepath,
     LoaderFunc                   loader,
@@ -80,8 +109,7 @@ std::shared_ptr<Image> STBImageLoader::loadImageData_(
     int32_t                      bitsPerChannel,
     bool                         isHdr) {
   int32_t width, height, channels;
-  // TODO: make it parameterized
-  int32_t desiredChannels = 0;
+  int32_t desiredChannels = 0;  // Load with the original number of channels
 
   auto* data = loader(filepath, &width, &height, &channels, desiredChannels);
   if (!data) {
@@ -90,16 +118,35 @@ std::shared_ptr<Image> STBImageLoader::loadImageData_(
     return nullptr;
   }
 
-  const size_t imageSize = static_cast<size_t>(width) * height * channels
-                         * (bitsPerChannel / CHAR_BIT);
+  size_t bytesPerChannel = bitsPerChannel / 8;
+  size_t bytesPerPixel   = bytesPerChannel * channels;
+  size_t imageSize       = static_cast<size_t>(width) * height * bytesPerPixel;
 
-  std::vector<std::byte> imageData(
-      reinterpret_cast<std::byte*>(data),
-      reinterpret_cast<std::byte*>(data) + imageSize);
+  std::vector<std::byte> pixels(reinterpret_cast<std::byte*>(data),
+                                reinterpret_cast<std::byte*>(data) + imageSize);
 
   freeFunc(data);
-  return std::make_shared<Image>(Image{
-    width, height, channels, bitsPerChannel, isHdr, std::move(imageData)});
+
+  Image image;
+  image.width     = static_cast<size_t>(width);
+  image.height    = static_cast<size_t>(height);
+  image.depth     = 1;
+  image.mipLevels = 1;
+  image.arraySize = 1;
+  image.dimension = ETextureType::TEXTURE_2D;
+  image.format    = determineFormat_(channels, bitsPerChannel, isHdr);
+  image.pixels    = std::move(pixels);
+
+  SubImage subImage;
+  subImage.width      = image.width;
+  subImage.height     = image.height;
+  subImage.rowPitch   = image.width * bytesPerPixel;
+  subImage.slicePitch = subImage.rowPitch * image.height;
+  subImage.pixelBegin = image.pixels.begin();
+
+  image.subImages.push_back(subImage);
+
+  return std::make_shared<Image>(std::move(image));
 }
 
 }  // namespace game_engine
