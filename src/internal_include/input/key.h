6d5464de85c16d8a4d94b49660c008dfdb2bb1cc
vadymchan <vadymchan@gmail.com>
2024-12-25 00:41:09 +0200

    Update key.h
    Defined new key codes and improved input enumerations.

--- src/internal_include/input/key.h
+++ src/internal_include/input/key.h
@@ -8,6 +8,7 @@ namespace game_engine {
 using PhysicalKey = SDL_Scancode;
 using VirtualKey  = SDL_Keycode;
 
+// TODO: why not use SDL_EventType?
 using KeyType = Uint32;  // SDL_KEYDOWN or SDL_KEYUP
 
 }  // namespace game_engine
