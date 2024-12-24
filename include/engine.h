a854234ba5379defb873a8f7eeb39750e835d05e
vadymchan <vadymchan@gmail.com>
2024-12-25 00:40:47 +0200

    Revise engine.h interface
    Updated function declarations and documentation for clarity.

--- include/engine.h
+++ include/engine.h
@@ -1,11 +1,15 @@
 #ifndef GAME_ENGINE_ENGINE_H
 #define GAME_ENGINE_ENGINE_H
 
-#include "event/application_event_handler.h"
+#include "config/config_manager.h"
+#include "config/runtime_settings.h"
+#include "ecs/component_loaders.h"
+#include "ecs/components/camera.h"
+#include "ecs/systems/camera_system.h"
+#include "ecs/systems/movement_system.h"
+#include "ecs/systems/render_system.h"
+#include "ecs/systems/system_manager.h"
 #include "event/application_event_manager.h"
-#include "event/keyboard_event_handler.h"
-#include "event/mouse_event_handler.h"
-#include "event/window_event_handler.h"
 #include "event/window_event_manager.h"
 #include "game.h"
 #include "gfx/rhi/dx12/rhi_dx12.h"
@@ -13,9 +17,22 @@
 #include "gfx/rhi/vulkan/rhi_vk.h"
 #include "input/input_manager.h"
 #include "platform/common/window.h"
+#include "resources/assimp_material_loader.h"
+#include "resources/assimp_render_model_loader.h"
+#include "scene/scene_manager.h"
+#include "utils/hot_reload/hot_reload_manager.h"
+#include "utils/image/image_loader_manager.h"
+#include "utils/image/image_manager.h"
 #include "utils/logger/console_logger.h"
 #include "utils/logger/global_logger.h"
+#include "utils/path_manager/path_manager.h"
+#include "utils/service/service_locator.h"
+#include "utils/third_party/directx_tex_util.h"
+#include "utils/third_party/stb_util.h"
 #include "utils/time/stopwatch.h"
+
+#include <utils/model/render_model_loader_manager.h>
+#include <utils/model/render_model_manager.h>
 
 #include <memory>
 
@@ -37,65 +54,182 @@ class Engine {
 
   // ======= END: public destructor   =========================================
 
+  // ======= BEGIN: public getters ============================================
+
+  auto getWindow() const -> const std::shared_ptr<Window>& { return m_window_; }
+
+  // ======= END: public getters   ============================================
+
+  // ======= BEGIN: public setters ============================================
+
+  void setGame(std::shared_ptr<Game> game) {
+    m_game_ = std::move(game);
+    m_game_->linkWithEngine(*this);
+  }
+
+  // ======= END: public setters   ============================================
+
   // ======= BEGIN: public misc methods =======================================
 
   // TODO: add config file
   auto init() -> bool {
-    bool successfullyInited{true};
+    bool successfullyInitialized{true};
 
     // logger
     // ------------------------------------------------------------------------
     m_consoleLogger_ = std::make_shared<ConsoleLogger>();
     GlobalLogger::AddLogger(m_consoleLogger_);
 
+    // input event
+    // ------------------------------------------------------------------------
+    auto keyboardEventHandler = std::make_shared<KeyboardEventHandler>();
+    auto mouseEventHandler    = std::make_shared<MouseEventHandler>();
+
+    // window event
+    // ------------------------------------------------------------------------
+    auto windowEventHandler = std::make_shared<WindowEventHandler>();
+    windowEventHandler->subscribe(
+        SDL_WINDOWEVENT_RESIZED, [this](const WindowEvent& event) {
+          this->m_window_->onResize(event);
+          // TODO: add here camera width and height change
+          auto scene = ServiceLocator::s_get<SceneManager>()->getCurrentScene();
+          auto& registry = scene->getEntityRegistry();
+          auto  view     = registry.view<Camera>();
+          auto  entity   = view.front();
+          if (entity != entt::null) {
+            auto& camera  = view.get<Camera>(entity);
+            camera.width  = event.data1;
+            camera.height = event.data2;
+          }
+
+          // TODO: refactor
+          g_rhi->onHandleResized(event.data1, event.data2, false);
+        });
+
+    // application event
+    // ------------------------------------------------------------------------
+    auto applicationEventHandler = std::make_shared<ApplicationEventHandler>();
+    applicationEventHandler->subscribe(
+        SDL_QUIT, std::bind(&Engine::onClose, this, std::placeholders::_1));
+    // [this](const ApplicationEvent& event) { this->onClose(event); }
+
+    // service locator
+    // ------------------------------------------------------------------------
+    ServiceLocator::s_provide<ConfigManager>();
+    ServiceLocator::s_provide<FileWatcherManager>();
+    ServiceLocator::s_provide<HotReloadManager>();
+    ServiceLocator::s_provide<InputManager>(keyboardEventHandler,
+                                            mouseEventHandler);
+    ServiceLocator::s_provide<WindowEventManager>(windowEventHandler);
+    ServiceLocator::s_provide<ApplicationEventManager>(applicationEventHandler);
+    ServiceLocator::s_provide<SceneManager>();
+    ServiceLocator::s_provide<SystemManager>();
+
+    // config
+    // ------------------------------------------------------------------------
+    auto configManager = ServiceLocator::s_get<ConfigManager>();
+    auto debugPath     = PathManager::s_getDebugPath() / "config.json";
+    configManager->addConfig(debugPath);
+    auto debugConfig = configManager->getConfig(debugPath).lock();
+
+    auto renderingApi = debugConfig->get<std::string>("renderingApi");
+
+    // register config
+    // ------------------------------------------------------------------------
+    debugConfig->registerConverter<math::Vector3Df>(
+        &math::g_getVectorfromConfig);
+    debugConfig->registerConverter<math::Quaternionf>(
+        &math::g_getQuaternionfromConfig);
+    debugConfig->registerConverter<CameraParametersDeprecated>(
+        &CameraParametersDeprecated::s_fromConfig);
+    debugConfig->registerConverter<Transform>(&LoadTransform);
+    debugConfig->registerConverter<Camera>(&LoadCamera);
+
+    // hot reload
+    // ------------------------------------------------------------------------
+    // auto hotReloadManager = ServiceLocator::s_get<HotReloadManager>();
+
     // window
     // ------------------------------------------------------------------------
     m_window_ = std::make_unique<Window>(
-        "First Triangle DirectX 12",
+        renderingApi,
         math::Dimension2Di{800, 600},
         math::Point2Di{100, 100},
         game_engine::Window::Flags::Resizable
             // TODO: Vulkan flag may not work for DX12 window
             | game_engine::Window::Flags::Vulkan);
-    // game
-    // ------------------------------------------------------------------------
-    m_game_ = std::make_unique<Game>(m_window_);
 
-    // input event
-    // ------------------------------------------------------------------------
-    m_keyboardEventHandler_ = std::make_shared<KeyboardEventHandler>();
-    m_mouseEventHandler_    = std::make_shared<MouseEventHandler>();
-    m_inputManager_ = std::make_unique<InputManager>(m_keyboardEventHandler_,
-                                                     m_mouseEventHandler_);
-    // window event
-    // ------------------------------------------------------------------------
-    m_windowEventHandler_ = std::make_shared<WindowEventHandler>();
-    m_windowEventHandler_->subscribe(
-        SDL_WINDOWEVENT_RESIZED, [this](const WindowEvent& event) {
-          this->m_window_->onResize(event);
-          this->m_game_->resize(event.data1, event.data2);  // TODO: refactor
-          g_rhi->onHandleResized(
-              event.data1, event.data2, false);             // TODO: refactor
-        });
-    m_windowEventManager_
-        = std::make_unique<WindowEventManager>(m_windowEventHandler_);
-
-    // application event
-    // ------------------------------------------------------------------------
-    m_applicationEventHandler_ = std::make_shared<ApplicationEventHandler>();
-    m_applicationEventHandler_->subscribe(
-        SDL_QUIT, std::bind(&Engine::onClose, this, std::placeholders::_1));
-    // [this](const ApplicationEvent& event) { this->onClose(event); }
-    m_applicationEventManager_
-        = std::make_unique<ApplicationEventManager>(m_applicationEventHandler_);
-
-    g_rhi = new RhiDx12();
+    if (renderingApi == "dx12") {
+      g_rhi = new RhiDx12();
+    } else if (renderingApi == "vulkan") {
+      g_rhi = new RhiVk();
+    } else {
+      g_rhi = new RhiVk();
+    }
     g_rhi->init(m_window_);
     g_rhi->onInitRHI();
 
-    m_game_->setup();
+    // image loader
+    // ------------------------------------------------------------------------
+    auto imageLoaderManager = std::make_shared<ImageLoaderManager>();
+    auto stbLoader          = std::make_shared<STBImageLoader>();
+    imageLoaderManager->registerLoader(EImageType::JPEG, stbLoader);
+    imageLoaderManager->registerLoader(EImageType::JPG, stbLoader);
+    imageLoaderManager->registerLoader(EImageType::PNG, stbLoader);
+    imageLoaderManager->registerLoader(EImageType::BMP, stbLoader);
+    imageLoaderManager->registerLoader(EImageType::TGA, stbLoader);
+    imageLoaderManager->registerLoader(EImageType::GIF, stbLoader);
+    imageLoaderManager->registerLoader(EImageType::HDR, stbLoader);
+    imageLoaderManager->registerLoader(EImageType::PIC, stbLoader);
+    imageLoaderManager->registerLoader(EImageType::PPM, stbLoader);
+    imageLoaderManager->registerLoader(EImageType::PGM, stbLoader);
+    auto directxTexLoader = std::make_shared<DirectXTexImageLoader>();
+    imageLoaderManager->registerLoader(EImageType::DDS, directxTexLoader);
+    ServiceLocator::s_provide<ImageLoaderManager>(imageLoaderManager);
 
-    return successfullyInited;
+    auto imageManager = std::make_shared<ImageManager>();
+    ServiceLocator::s_provide<ImageManager>(imageManager);
+
+    // model / material loader
+    // ------------------------------------------------------------------------
+    auto renderModelLoaderManager
+        = std::make_shared<RenderModelLoaderManager>();
+    auto assimpModelLoader = std::make_shared<AssimpRenderModelLoader>();
+    renderModelLoaderManager->registerLoader(EModelType::OBJ,
+                                             assimpModelLoader);
+    renderModelLoaderManager->registerLoader(EModelType::FBX,
+                                             assimpModelLoader);
+    ServiceLocator::s_provide<RenderModelLoaderManager>(
+        renderModelLoaderManager);
+
+    auto renderModelManager = std::make_shared<RenderModelManager>();
+    ServiceLocator::s_provide<RenderModelManager>(renderModelManager);
+
+    // ecs
+    // ------------------------------------------------------------------------
+    auto systemManager = ServiceLocator::s_get<SystemManager>();
+    systemManager->addSystem(std::make_shared<CameraSystem>());
+    systemManager->addSystem(std::make_shared<MovementSystem>());
+    systemManager->addSystem(std::make_shared<RenderSystem>());
+
+    // renderer
+    // ------------------------------------------------------------------------
+    m_renderer_ = std::make_shared<Renderer>();
+
+    return successfullyInitialized;
+  }
+
+  void render() {
+    // TODO: temp solution to create render context here and each frame
+    // (consider other approach)
+    auto renderContext = std::make_shared<RenderContext>();
+
+    renderContext->scene
+        = ServiceLocator::s_get<SceneManager>()->getCurrentScene();
+    renderContext->viewportWidth  = m_window_->getSize().width();
+    renderContext->viewportHeight = m_window_->getSize().height();
+
+    m_renderer_->renderFrame(renderContext);
   }
 
   void run() {
@@ -109,8 +243,7 @@ class Engine {
       processEvents_();
       m_game_->processInput();  // TODO: consider remove
       update_(deltaTime);
-      m_game_->draw();
-      // render();
+      render();
     }
   }
 
@@ -139,13 +272,17 @@ class Engine {
   void processEvents_() {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
-      m_inputManager_->routeEvent(event);
-      m_windowEventManager_->routeEvent(event);
-      m_applicationEventManager_->routeEvent(event);
+      ServiceLocator::s_get<InputManager>()->routeEvent(event);
+      ServiceLocator::s_get<WindowEventManager>()->routeEvent(event);
+      ServiceLocator::s_get<ApplicationEventManager>()->routeEvent(event);
     }
   }
 
   void update_(float deltaTime) {
+    auto systemManager = ServiceLocator::s_get<SystemManager>();
+    auto scene = ServiceLocator::s_get<SceneManager>()->getCurrentScene();
+    systemManager->updateSystems(scene, deltaTime);
+
     m_game_->update(deltaTime);
 
     Shader::s_startAndRunCheckUpdateShaderThread();
@@ -159,18 +296,11 @@ class Engine {
   DeltaTime                      m_deltaTime_;
   std::shared_ptr<ConsoleLogger> m_consoleLogger_;
   std::shared_ptr<Window>        m_window_;
+  std::shared_ptr<Renderer>      m_renderer_;
 
-  std::shared_ptr<KeyboardEventHandler> m_keyboardEventHandler_;
-  std::shared_ptr<MouseEventHandler>    m_mouseEventHandler_;
-  std::unique_ptr<InputManager>         m_inputManager_;
-
-  std::shared_ptr<WindowEventHandler> m_windowEventHandler_;
-  std::unique_ptr<WindowEventManager> m_windowEventManager_;
-
-  std::shared_ptr<ApplicationEventHandler> m_applicationEventHandler_;
-  std::unique_ptr<ApplicationEventManager> m_applicationEventManager_;
-
-  std::unique_ptr<Game> m_game_;
+  // TODO: consider adding interface for Game class (for abstraction). add objec
+  // in init method parameter
+  std::shared_ptr<Game> m_game_;
   // TODO: memory manager
 
   // ======= END: private misc fields   =======================================
@@ -178,4 +308,4 @@ class Engine {
 
 }  // namespace game_engine
 
-#endif
+#endif  // GAME_ENGINE_ENGINE_H
