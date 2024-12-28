45b4c3b455417d68388e6d358806e9307ee037f6
vadymchan <vadymchan@gmail.com>
2024-12-28 13:42:51 +0200

    refactor dxc code so now it supports both DXIL and SPIR-V

--- src/internal_include/gfx/rhi/dx12/dxc_util.h
+++ src/internal_include/gfx/rhi/dx12/dxc_util.h
@@ -1,216 +1,310 @@
 #ifndef GAME_ENGINE_DXC_UTIL_DX_H
 #define GAME_ENGINE_DXC_UTIL_DX_H
 
-// TODO: all this class is need to be refactored completely!!!
-
-#include "platform/windows/windows_platform_setup.h"
-
-#ifdef GAME_ENGINE_RHI_DX12
-
 #include "gfx/rhi/rhi_type.h"
-#include "platform/windows/windows_platform_setup.h"
 #include "utils/logger/global_logger.h"
 
+#include <cstdint>
 #include <filesystem>
 #include <fstream>
+#include <memory>
 #include <sstream>
 #include <string>
 #include <vector>
 
+#if defined(_WIN32)
+#include <Windows.h>
+#define DXC_COMPILER_LIBRARY L"dxcompiler.dll"
+#define GET_DXC_SYMBOL       GetProcAddress
+using DxcLibHandle = HMODULE;
+#else
+#include <dlfcn.h>
+#if defined(__APPLE__)
+#define DXC_COMPILER_LIBRARY "libdxcompiler.dylib"
+#elif defined(__linux__)
+#define DXC_COMPILER_LIBRARY "libdxcompiler.so"
+#else
+#error \
+    "Unsupported platform. Please define DXC_COMPILER_LIBRARY for your platform."
+#endif
+#define GET_DXC_SYMBOL dlsym
+using DxcLibHandle = void*;
+#endif
+
+#include <dxc/dxcapi.h>
+
 namespace game_engine {
 
-namespace fs = std::filesystem;
+enum class ShaderBackend {
+  DXIL,
+  SPIRV
+};
+
+struct OptionalShaderParams {
+  // include directories passed to -I
+  std::vector<std::wstring> includeDirs;
+  // macro definitions passed to -D
+  std::vector<std::wstring> preprocessorDefs;
+  // additional flags
+  std::vector<std::wstring> extraArgs;
+};
+
+// helper function to wrap any DXC/COM pointer in a std::shared_ptr
+template <typename T>
+std::shared_ptr<T> MakeDxcSharedPtr(T* rawPtr) {
+  // Custom deleter calls ->Release() when refcount hits zero
+  return std::shared_ptr<T>(rawPtr, [](T* p) {
+    if (p) {
+      p->Release();
+    }
+  });
+}
 
 class DxcUtil {
   public:
-  // ======= BEGIN: public static methods =====================================
-
-  // TODO: consider whether this is the good naming convention
-  static DxcUtil& s_getInstance(const wchar_t* dllName = L"dxcompiler.dll",
-                                const char*    fnName  = "DxcCreateInstance") {
+  static DxcUtil& s_get() {
     static DxcUtil instance;
-    // TODO: not good solution
-    instance.initialize(dllName, fnName);
+    if (!instance.initialize()) {
+      GlobalLogger::Log(LogLevel::Error, "Failed to initialize DXC library.");
+    }
+
     return instance;
   }
 
-  // ======= END: public static methods   =====================================
-
-  // ======= BEGIN: public misc methods =======================================
-
-  HRESULT initialize(const wchar_t* dllName = L"dxcompiler.dll",
-                     const char*    fnName  = "DxcCreateInstance") {
-    if (m_dll_) {
-      return S_OK;
-    }
-
-    m_dll_ = LoadLibraryW(dllName);
-    if (!m_dll_) {
-      return HRESULT_FROM_WIN32(GetLastError());
-    }
-
-    m_createFn_ = (DxcCreateInstanceProc)GetProcAddress(m_dll_, fnName);
-    if (!m_createFn_) {
-      HRESULT hr = HRESULT_FROM_WIN32(GetLastError());
-      FreeLibrary(m_dll_);
-      m_dll_ = nullptr;
-      return hr;
-    }
-
-    return S_OK;
-  }
-
-  void release() {
-    if (m_dll_) {
-      m_createFn_ = nullptr;
-      FreeLibrary(m_dll_);
-      m_dll_ = nullptr;
-    }
-  }
-
-  ComPtr<IDxcBlob> compileHlslFileToDxil(const fs::path&     shaderFilePath,
-                                         const std::wstring& shaderProfile,
-                                         const std::wstring& entryPoint) {
-    std::string hlslCode = s_readShaderFile_(shaderFilePath);
-    return compileHlslCodeToDxil(hlslCode, shaderProfile, entryPoint);
-  }
-
-  ComPtr<IDxcBlob> compileHlslCodeToDxil(const std::string&  hlslCode,
-                                         const std::wstring& shaderProfile,
-                                         const std::wstring& entryPoint) {
-    ComPtr<IDxcCompiler3>      compiler;
-    ComPtr<IDxcUtils>          utils;
-    ComPtr<IDxcIncludeHandler> includeHandler;
-
-    if (FAILED(createInstance_(CLSID_DxcCompiler, IID_PPV_ARGS(&compiler)))
-        || FAILED(createInstance_(CLSID_DxcUtils, IID_PPV_ARGS(&utils)))
-        || FAILED(utils->CreateDefaultIncludeHandler(&includeHandler))) {
-      GlobalLogger::Log(LogLevel::Error, "Failed to create DXC instances");
-      return nullptr;
-    }
-
-    DxcBuffer sourceBuffer;
-    sourceBuffer.Ptr      = hlslCode.data();
-    sourceBuffer.Size     = hlslCode.size();
-    sourceBuffer.Encoding = DXC_CP_ACP;
-
-    std::vector<LPCWSTR> arguments = {
-      L"-E",
-      entryPoint.c_str(),           // Entry point.
-      L"-T",
-      shaderProfile.c_str(),        // Shader model.
-      DXC_ARG_WARNINGS_ARE_ERRORS,  // Warnings as errors.
-      DXC_ARG_DEBUG,                // Enable debug information.
-#ifdef _DEBUG
-      L"-Zi",                       // Debug info.
-      L"-Qembed_debug",             // Embed PDB in shader container.
-      L"-Od"                        // Disable optimization.
-#else
-      L"-O3"  // Optimization Level 3 (Default).
-#endif
-    };
-
-    ComPtr<IDxcResult> result;
-    if (FAILED(compiler->Compile(&sourceBuffer,
-                                 arguments.data(),
-                                 arguments.size(),
-                                 includeHandler.Get(),
-                                 IID_PPV_ARGS(&result)))) {
-      GlobalLogger::Log(LogLevel::Error, "Failed to compile shader");
-      return nullptr;
-    }
-
-    ComPtr<IDxcBlobUtf8> errors;
-    result->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&errors), nullptr);
-
-    if (errors != nullptr && errors->GetStringLength() > 0) {
-      GlobalLogger::Log(LogLevel::Warning, errors->GetStringPointer());
-    }
-
-    HRESULT resultCode;
-    if (FAILED(result->GetStatus(&resultCode))) {
+  // read from disk, then compile
+  std::shared_ptr<IDxcBlob> compileHlslFile(
+      const std::filesystem::path& shaderPath,
+      EShaderAccessStageFlag       stage,
+      const std::wstring&          entryPoint,
+      ShaderBackend                backend,
+      const OptionalShaderParams&  optionalParams = {}) {
+    std::string code = readFile_(shaderPath);
+    if (code.empty()) {
       GlobalLogger::Log(LogLevel::Error,
-                        "Compilation failed with status error");
+                        "Failed to read shader file: " + shaderPath.string());
       return nullptr;
     }
-
-    ComPtr<IDxcBlob> shader;
-    if (FAILED(result->GetOutput(
-            DXC_OUT_OBJECT, IID_PPV_ARGS(&shader), nullptr))) {
-      GlobalLogger::Log(LogLevel::Error, "Failed to get shader blob");
-      return nullptr;
-    }
-
-    return shader;
+    return compileHlslCode(code, stage, entryPoint, backend, optionalParams);
   }
 
-  // ======= END: public misc methods   =======================================
+  // compile from a raw string
+  std::shared_ptr<IDxcBlob> compileHlslCode(
+      const std::string&          hlslCode,
+      EShaderAccessStageFlag      stage,
+      const std::wstring&         entryPoint,
+      ShaderBackend               backend,
+      const OptionalShaderParams& optionalParams = {}) {
+    if (!m_dxcCreateFn) {
+      GlobalLogger::Log(LogLevel::Error, "DXC library not loaded properly.");
+      return nullptr;
+    }
+
+    std::wstring targetProfile = getTargetProfile_(stage);
+    if (targetProfile.empty()) {
+      GlobalLogger::Log(LogLevel::Error, "Invalid shader stage provided.");
+      return nullptr;
+    }
+
+    GlobalLogger::Log(
+        LogLevel::Info,
+        "Compiling shader for target: "
+            + std::string(targetProfile.begin(), targetProfile.end()));
+
+    IDxcCompiler3* compilerRaw = nullptr;
+    IDxcUtils*     utilsRaw    = nullptr;
+    if (FAILED(m_dxcCreateFn(CLSID_DxcCompiler, IID_PPV_ARGS(&compilerRaw)))
+        || FAILED(m_dxcCreateFn(CLSID_DxcUtils, IID_PPV_ARGS(&utilsRaw)))) {
+      GlobalLogger::Log(LogLevel::Error, "Failed to create DXC instances.");
+      return nullptr;
+    }
+
+    auto compiler = MakeDxcSharedPtr(compilerRaw);
+    auto utils    = MakeDxcSharedPtr(utilsRaw);
+
+    IDxcIncludeHandler* includeHandlerRaw = nullptr;
+    if (FAILED(utils->CreateDefaultIncludeHandler(&includeHandlerRaw))) {
+      return nullptr;
+    }
+    auto includeHandler = MakeDxcSharedPtr(includeHandlerRaw);
+
+    DxcBuffer sourceBuf;
+    sourceBuf.Ptr      = hlslCode.data();
+    sourceBuf.Size     = hlslCode.size();
+    sourceBuf.Encoding = DXC_CP_UTF8;
+
+    std::vector<std::wstring> argStrings;
+    std::vector<LPCWSTR>      args;
+
+    argStrings.push_back(L"-E");
+    argStrings.push_back(entryPoint);
+    argStrings.push_back(L"-T");
+    argStrings.push_back(targetProfile);
+
+#ifdef _DEBUG
+    argStrings.push_back(L"-Zi");
+    argStrings.push_back(L"-Qembed_debug");
+    argStrings.push_back(L"-Od");
+#else
+    argStrings.push_back(L"-O3");
+#endif
+
+    if (backend == ShaderBackend::SPIRV) {
+      argStrings.push_back(L"-spirv");
+    }
+
+    // includes
+    for (auto& inc : optionalParams.includeDirs) {
+      argStrings.push_back(L"-I");
+      argStrings.push_back(inc);
+    }
+    // defines
+    for (auto& def : optionalParams.preprocessorDefs) {
+      argStrings.push_back(L"-D");
+      argStrings.push_back(def);
+    }
+    // extra
+    for (auto& ex : optionalParams.extraArgs) {
+      argStrings.push_back(ex);
+    }
+
+    // gather pointers
+    args.reserve(argStrings.size());
+    for (auto& s : argStrings) {
+      args.push_back(s.c_str());
+    }
+
+    IDxcResult* resultRaw = nullptr;
+    HRESULT     hr        = compiler->Compile(&sourceBuf,
+                                   args.data(),
+                                   (UINT)args.size(),
+                                   includeHandler.get(),
+                                   IID_PPV_ARGS(&resultRaw));
+    if (FAILED(hr) || !resultRaw) {
+      GlobalLogger::Log(LogLevel::Error, "Failed to compile shader.");
+      return nullptr;
+    }
+    auto result = MakeDxcSharedPtr(resultRaw);
+
+    IDxcBlobUtf8* errorsRaw = nullptr;
+    hr = result->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&errorsRaw), nullptr);
+    auto errorsPtr = MakeDxcSharedPtr(errorsRaw);
+    if (errorsPtr && errorsPtr->GetStringLength() > 0) {
+      GlobalLogger::Log(LogLevel::Warning,
+                        std::string(errorsPtr->GetStringPointer(),
+                                    errorsPtr->GetStringPointer()
+                                        + errorsPtr->GetStringLength()));
+    }
+
+    HRESULT status;
+    result->GetStatus(&status);
+    if (FAILED(status)) {
+      GlobalLogger::Log(LogLevel::Error, "Shader compilation failed.");
+      return nullptr;
+    }
+
+    IDxcBlob* blobRaw = nullptr;
+    hr = result->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&blobRaw), nullptr);
+    if (FAILED(hr) || !blobRaw) {
+      GlobalLogger::Log(LogLevel::Error, "Failed to retrieve shader blob.");
+      return nullptr;
+    }
+
+    return MakeDxcSharedPtr(blobRaw);
+  }
 
   private:
-  // ======= BEGIN: private static methods ====================================
-
-  static std::string s_readShaderFile_(const fs::path& shaderFilePath) {
-    std::ifstream shaderFile(shaderFilePath);
-    if (!shaderFile) {
-      GlobalLogger::Log(
-          LogLevel::Error,
-          "Failed to open shader file: " + shaderFilePath.string());
-      return "";
+  bool initialize() {
+    if (m_libHandle) {
+      return true;
     }
 
-    std::stringstream shaderStream;
-    shaderStream << shaderFile.rdbuf();
-    return shaderStream.str();
+#if defined(_WIN32)
+    m_libHandle = LoadLibraryW(DXC_COMPILER_LIBRARY);
+#else
+    m_libHandle = dlopen(DXC_COMPILER_LIBRARY, RTLD_LAZY);
+#endif
+
+    if (!m_libHandle) {
+      return false;
+    }
+
+    m_dxcCreateFn = reinterpret_cast<DxcCreateInstanceProc>(
+        GET_DXC_SYMBOL(m_libHandle, "DxcCreateInstance"));
+
+    if (!m_dxcCreateFn) {
+#if defined(_WIN32)
+      FreeLibrary(m_libHandle);
+#else
+      dlclose(m_libHandle);
+#endif
+      m_libHandle = nullptr;
+      return false;
+    }
+
+    return true;
   }
 
-  // ======= END: private static methods   ====================================
+  std::wstring getTargetProfile_(EShaderAccessStageFlag stage) {
+    static const std::wstring suffix = L"_6_6";
+    switch (stage) {
+      case EShaderAccessStageFlag::VERTEX:
+        return L"vs" + suffix;
+      case EShaderAccessStageFlag::FRAGMENT:
+        return L"ps" + suffix;
+      case EShaderAccessStageFlag::COMPUTE:
+        return L"cs" + suffix;
+      case EShaderAccessStageFlag::GEOMETRY:
+        return L"gs" + suffix;
+      case EShaderAccessStageFlag::TESSELLATION_CONTROL:
+        return L"hs" + suffix;
+      case EShaderAccessStageFlag::TESSELLATION_EVALUATION:
+        return L"ds" + suffix;
 
-  // ======= BEGIN: private constructors ======================================
-
-  DxcUtil()               = default;
-  DxcUtil(const DxcUtil&) = delete;
-
-  // ======= END: private constructors   ======================================
-
-  // ======= BEGIN: private destructors ========================================
-
-  ~DxcUtil() { release(); }
-
-  // ======= END: private destructors   ========================================
-
-  // ======= BEGIN: private misc methods ======================================
-
-  HRESULT createInstance_(REFCLSID clsid, REFIID iid, void** result) {
-    if (!result) {
-      return E_POINTER;
+      case EShaderAccessStageFlag::RAYTRACING:
+      case EShaderAccessStageFlag::RAYTRACING_RAYGEN:
+      case EShaderAccessStageFlag::RAYTRACING_MISS:
+      case EShaderAccessStageFlag::RAYTRACING_CLOSESTHIT:
+      case EShaderAccessStageFlag::RAYTRACING_ANYHIT:
+        return L"lib" + suffix;
+      default:
+        return L"";  // unsupported combination
     }
-    if (!m_dll_) {
-      return E_FAIL;
-    }
-    if (!m_createFn_) {
-      return E_FAIL;
-    }
-    return m_createFn_(clsid, iid, result);
   }
 
-  // ======= END: private misc methods   ======================================
+  static std::string readFile_(const std::filesystem::path& path) {
+    std::ifstream ifs(path, std::ios::binary);
+    if (!ifs) {
+      GlobalLogger::Log(LogLevel::Error,
+                        "Failed to open shader file: " + path.string());
+      return {};
+    }
+    std::stringstream ss;
+    ss << ifs.rdbuf();
+    return ss.str();
+  }
 
-  // ======= BEGIN: private overloaded operators ==============================
+  private:
+  DxcUtil() = default;
 
+  ~DxcUtil() {
+    if (m_libHandle) {
+#if defined(_WIN32)
+      FreeLibrary(m_libHandle);
+#else
+      dlclose(m_libHandle);
+#endif
+      m_libHandle = nullptr;
+    }
+  }
+
+  DxcUtil(const DxcUtil&)            = delete;
   DxcUtil& operator=(const DxcUtil&) = delete;
 
-  // ======= END: private overloaded operators   ==============================
-
-  // ======= BEGIN: private misc fields =======================================
-
-  HMODULE               m_dll_      = nullptr;
-  DxcCreateInstanceProc m_createFn_ = nullptr;
-
-  // ======= END: private misc fields   =======================================
+  private:
+  DxcLibHandle          m_libHandle   = nullptr;
+  DxcCreateInstanceProc m_dxcCreateFn = nullptr;
 };
 
 }  // namespace game_engine
-
-#endif  // GAME_ENGINE_RHI_DX12
 
 #endif  // GAME_ENGINE_DXC_UTIL_DX_H
